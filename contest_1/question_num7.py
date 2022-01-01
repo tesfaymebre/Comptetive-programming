1245b438ee93647222dc37391e9f7b709a81b714
tesfaymebre <tesfaymebre@gmail.com>
2022-01-01 05:15:38 +0300

    A2SV

--- contest_1/question_num7.py
+++ contest_1/question_num7.py
@@ -2,5 +2,8 @@ symbol_val = {'I':1, 'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
 S = input()
 numVal = 0
 for i in range(len(S)):
-    numVal = numVal + int(symbol_val[S[i]])
+    if  i != (len(S)-1) and symbol_val[S[i]] < symbol_val[S[i+1]]:
+            numVal = numVal - int(symbol_val[S[i]])
+    else:
+        numVal = numVal + int(symbol_val[S[i]])
 print(numVal)
---
+++ week 2/56_Merge_Intervals.py
@@ -0,0 +1,20 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda a:a[0])
+        merged = []
+        start = intervals[0][0]
+        end = intervals[0][1]
+        for i in range(len(intervals)):
+            if intervals[i][0] <= end:
+                if intervals[i][1] >= end:
+                    end = intervals[i][1]
+                if i == len(intervals)-1:
+                    merged.append([start,end])
+            else:
+                merged.append([start,end])
+                start = intervals[i][0]
+                end = intervals[i][1]
+                if i == len(intervals)-1:
+                    merged.append([start,end])
+        return merged
+                    
