08e8c969d8f0e7a44b74569050b263c5d5ea2d36
tesfaymebre <tesfaymebre@gmail.com>
2022-01-10 10:22:25 +0300

    classes

---
+++ everyDayPractice/Classes_Dealing_with_Complex_Numbers.py
@@ -0,0 +1,37 @@
+import math
+
+class Complex(object):
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
+    def __add__(self, no):
+        return Complex(self.real + no.real,
+                       self.imaginary + no.imaginary)
+    def __sub__(self, no):
+        return Complex(self.real - no.real,
+                       self.imaginary - no.imaginary)
+    def __mul__(self, no):
+        return Complex(self.real*no.real - self.imaginary*no.imaginary,
+                       self.real*no.imaginary + self.imaginary*no.real)
+    def __truediv__(self, no):
+        realNum = self.real*no.real + self.imaginary*no.imaginary
+        imagNum = self.imaginary*no.real- self.real*no.imaginary
+        denom = no.real**2 +no.imaginary**2
+        return Complex(realNum/denom,imagNum/denom)
+    def mod(self):
+        return Complex(math.sqrt(self.real**2 + self.imaginary**2),0)
+    def __str__(self):
+        if self.imaginary == 0:
+            result = "%.2f+0.00i" % (self.real)
+        elif self.real == 0:
+            if self.imaginary >= 0:
+                result = "0.00+%.2fi" % (self.imaginary)
+            else:
+                result = "0.00-%.2fi" % (abs(self.imaginary))
+        elif self.imaginary > 0:
+            result = "%.2f+%.2fi" % (self.real, self.imaginary)
+        else:
+            result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
+        return result
+
+if __name__ == '__main__':
