5747588eef571a668316875f3cfc1151a935ecb1
tesfaymebre <95352714+tesfaymebre@users.noreply.github.com>
2022-02-13 23:19:15 +0300

    Time: 193 ms (64.23%), Space: 15.5 MB (32.30%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        p1 = 0
+        p2 = 1
+        
+        while p2 < len(nums):
+            if nums[p1] == 0 and nums[p2] == 0:
+                p2 += 1
+                continue
+            elif nums[p1] == 0 and nums[p2] != 0:
+                nums[p1], nums[p2] = nums[p2], nums[p1]
+                
+            p1 += 1
+            p2 += 1
