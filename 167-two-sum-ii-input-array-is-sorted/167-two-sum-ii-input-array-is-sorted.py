9df74b90e30e448e2d6ed74c14196d517606e569
tesfaymebre <95352714+tesfaymebre@users.noreply.github.com>
2022-02-14 21:58:19 +0300

    Time: 224 ms (9.43%), Space: 14.9 MB (18.68%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -3,7 +3,7 @@ class Solution:
         p1 = 0
         p2 = len(numbers) - 1
         
-        while(p2 > p1):
+        while True:
             
             if numbers[p2] + numbers[p1] > target:
                 p2 -= 1
