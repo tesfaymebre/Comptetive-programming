ed8c7d96e681ad5a41ded843d17436aab1c8cfa2
tesfaymebre <95352714+tesfaymebre@users.noreply.github.com>
2022-02-23 23:07:05 +0300

    Time: 1548 ms (81.46%), Space: 28 MB (79.79%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -9,11 +9,16 @@ class Solution:
         prefix_sum.append(0)
         
         for i in range(len(nums)):
+            
             prefix_sum.append(prefix_sum[-1] + nums[i])
             gap = (i - ptr) * nums[i] - (prefix_sum[i] - prefix_sum[ptr])
+            
             while ptr < i and gap > k:
+                
                 ptr += 1
                 gap = (i - ptr) * nums[i] - (prefix_sum[i] - prefix_sum[ptr])
+                
             ans = max(ans, i - ptr + 1)
+            
         return ans
         
