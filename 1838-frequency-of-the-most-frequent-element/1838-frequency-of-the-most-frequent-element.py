47d3dfd7dd4bcdff39cfcf138fb47800004893a7
tesfaymebre <95352714+tesfaymebre@users.noreply.github.com>
2022-02-23 23:00:06 +0300

    Time: 2878 ms (15.19%), Space: 28.2 MB (58.51%) - LeetHub

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        
+        ans = 0
+        ptr = 0
+        prefix_sum = []
+        
+        prefix_sum.append(0)
+        
+        for i in range(len(nums)):
+            prefix_sum.append(prefix_sum[-1] + nums[i])
+            gap = (i - ptr) * nums[i] - (prefix_sum[i] - prefix_sum[ptr])
+            while ptr < i and gap > k:
+                ptr += 1
+                gap = (i - ptr) * nums[i] - (prefix_sum[i] - prefix_sum[ptr])
+            ans = max(ans, i - ptr + 1)
+        return ans
+        
