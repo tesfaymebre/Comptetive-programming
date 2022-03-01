b67ba1b5bca1c3b4767516dc16d002e7080a606b
tesfaymebre <95352714+tesfaymebre@users.noreply.github.com>
2022-03-01 15:24:08 +0300

    Time: 57 ms (20.75%), Space: 13.9 MB (86.17%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -6,31 +6,49 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        if not root.left and not root.right:
-            return root.val
+        self.total = 0
         
-        stack = []
-        total = 0
-        visited = set()
-        
-        stack.append(root)
-        
-        while stack:
+        def recur(node,value):
+            if not node.left and not node.right:
+                self.total += int(value)
+                return 
             
-            if stack[-1].left and stack[-1].left not in visited:
-                stack.append(stack[-1].left)
-            elif stack[-1].right and stack[-1].right not in visited:
-                stack.append(stack[-1].right)
-            elif stack[-1].left in visited or stack[-1].right in visited:
-                visited.add(stack.pop())
-            else:
-                st = ""
-                for x in stack:
-                    st+= str(x.val)
-                total += int(st)
-                visited.add(stack.pop())
+            if node.left:
+                recur(node.left, value + str(node.left.val))
+                
+            if node.right:
+                recur(node.right, value + str(node.right.val))
         
-        return total
+        recur(root,str(root.val))
+        
+        return self.total   
+            
+        
+#         if not root.left and not root.right:
+#             return root.val
+        
+#         stack = []
+#         total = 0
+#         visited = set()
+        
+#         stack.append(root)
+        
+#         while stack:
+            
+#             if stack[-1].left and stack[-1].left not in visited:
+#                 stack.append(stack[-1].left)
+#             elif stack[-1].right and stack[-1].right not in visited:
+#                 stack.append(stack[-1].right)
+#             elif stack[-1].left in visited or stack[-1].right in visited:
+#                 visited.add(stack.pop())
+#             else:
+#                 st = ""
+#                 for x in stack:
+#                     st+= str(x.val)
+#                 total += int(st)
+#                 visited.add(stack.pop())
+        
+#         return total
                 
             
             
