9bdd5fcebaf26e62e39cf991e1e037934415303c
tesfaymebre <95352714+tesfaymebre@users.noreply.github.com>
2022-03-10 16:11:45 +0300

    Time: 708 ms (65.33%), Space: 16 MB (54.97%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -5,17 +5,15 @@ class Solution:
                 return
             
             grid[row][col] = 0
-            self.count += 1
             for direction in DIR:
                 new_row, new_col = row + direction[0], col + direction[1]
+                
                 if in_bound(new_row,new_col):
-                    print(new_row,new_col,"ss")
                     dfs(new_row,new_col)
             
         DIR = [[1,0],[0,1],[-1,0],[0,-1]]
         in_bound = lambda row, col: 0 <= row < len(grid) and 0 <= col < len(grid[row])
         
-        self.count = 0
         for i in range(len(grid[0]) - 1):
             dfs(0, i)
             dfs(len(grid) - 1, i)
@@ -24,10 +22,5 @@ class Solution:
             dfs(j,0)
             dfs(j, len(grid[0]) - 1)
         
-        ones = 0
-        for k in range(len(grid)):
-            for l in range(len(grid[k])):
-                if grid[k][l] == 1:
-                    ones += 1
-        return ones
+        return sum(sum(rws) for rws in grid)
                 
