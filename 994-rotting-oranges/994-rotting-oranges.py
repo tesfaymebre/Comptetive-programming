6bcfd6ff72bc7e26c46c679caabe42f3fd838856
tesfaymebre <95352714+tesfaymebre@users.noreply.github.com>
2022-03-14 10:35:15 +0300

    Time: 68 ms (62.63%), Space: 13.9 MB (95.14%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -3,33 +3,30 @@ class Solution:
         DIR = [[0,1],[1,0],[0,-1],[-1,0]]
         in_bound = lambda row,col: 0 <= row < len(grid) and 0 <= col < len(grid[row])
         queue = deque()
-        visited = set()
-        time = 0
+        timer = 0
         fresh = 0
         
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == 2:
                     queue.append((i,j))
-                    visited.add((i,j))
-                if grid[i][j] == 1:
+                elif grid[i][j] == 1:
                     fresh += 1
+                    
         while queue and fresh:
             n = len(queue)
-            time += 1
+            timer += 1
             
             for k in range(n):
                 curr = queue.popleft()
                 for direction in DIR:
                     new_row, new_col = curr[0] + direction[0], curr[1] + direction[1] 
-                    if (new_row,new_col) not in visited and in_bound(new_row,new_col) and \
-                        grid[new_row][new_col] == 1:
-                        queue.append((new_row,new_col))
-                        visited.add((new_row, new_col)) 
+                    if in_bound(new_row,new_col) and grid[new_row][new_col] == 1:
+                        grid[new_row][new_col] = 2
+                        queue.append((new_row,new_col)) 
                         fresh -= 1
-        if fresh:
-            return -1
-        return time
+                        
+        return timer if not fresh else -1
                    
         
         
