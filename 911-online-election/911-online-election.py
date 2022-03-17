da76c2758abe874a58e898e61d41b6755ebf1baf
tesfaymebre <95352714+tesfaymebre@users.noreply.github.com>
2022-03-17 16:52:25 +0300

    Time: 860 ms (68.85%), Space: 20.1 MB (21.86%) - LeetHub

--- 911-online-election/911-online-election.py
+++ 911-online-election/911-online-election.py
@@ -1,29 +1,34 @@
 class TopVotedCandidate:
 
     def __init__(self, persons: List[int], times: List[int]):
-        self.persons = persons
         self.times = times
         
         freq = defaultdict(int)
         self.curr_lead = dict()
-        leader = self.persons[0]
-        for i in range(len(self.persons)):
-            freq[self.persons[i]] += 1
-            if freq[leader] <= freq[self.persons[i]]:
-                leader = self.persons[i]
+        leader = persons[0]
+        
+        for i in range(len(persons)):
+            freq[persons[i]] += 1
+            
+            if freq[leader] <= freq[persons[i]]:
+                leader = persons[i]
+                
             self.curr_lead[i] = leader
 
     def q(self, t: int) -> int:
         indx = 0
         left = 0
-        right = len(self.persons) - 1
+        right = len(self.times) - 1
+        
         while left <= right:
             mid = left + (right - left)//2
+            
             if self.times[mid] <= t:
                 left = mid + 1
                 indx = mid
             else:
                 right = mid - 1
+                
         return self.curr_lead[indx]
 
 # Your TopVotedCandidate object will be instantiated and called as such:
