3bfba05c5ca4989502d1fac1fdee93a426f62166
tesfaymebre <95352714+tesfaymebre@users.noreply.github.com>
2022-04-12 10:55:19 +0300

    Add files via upload

---
+++ Quality_vs_Quantity.py
@@ -0,0 +1,25 @@
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    sequences = list(map(int,input().split()))
+    sequences.sort()
+    red_tot = 0
+    blue_tot = sequences[0]
+
+    for j in range(1, n//2 + 1):
+        blue_tot += sequences[j]
+        red_tot += sequences[-j]
+        
+        if red_tot > blue_tot:
+            print("YES")
+            break
+    if red_tot <= blue_tot:
+        print("NO")
+
+    # len = n // 2 + 1
+
+    # if sum(sequences[:len:1]) < sum(sequences[len::]):
+    #     print("YES")
+    # else:
+    #     print("NO")
