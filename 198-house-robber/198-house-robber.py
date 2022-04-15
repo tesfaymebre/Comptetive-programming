951dbfe467ae3ec48b69f5df1795bf3a2400f60a
tesfaymebre <95352714+tesfaymebre@users.noreply.github.com>
2022-04-15 18:58:22 +0300

    Time: 40 ms (65.26%), Space: 13.9 MB (70.42%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,11 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        prev = 0
-        curr = 0
+        memo = {}
         
-        for x in nums:
-            temp = prev
-            prev = curr
-            curr = max(temp + x, curr)
+        def dp(i):
+            if i >= len(nums):
+                return 0
+            if i in memo:
+                return memo[i]
             
-        return curr
+            memo[i] = max(nums[i] + dp(i+2), dp(i+1))
+            return memo[i]
+        
+        return dp(0)
