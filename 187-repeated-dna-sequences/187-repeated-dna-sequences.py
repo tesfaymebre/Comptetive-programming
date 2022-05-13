70a80db20685d097e1df8f716c6c53ef61051c06
tesfaymebre <95352714+tesfaymebre@users.noreply.github.com>
2022-05-13 18:49:03 +0300

    Time: 512 ms (5.13%), Space: 33.8 MB (5.05%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.py
@@ -4,13 +4,31 @@ class Solution:
         ans = []
         j = 10
         
+        def getMask(seq):
+            mask = [0,0,0,0]
+            
+            for i in range(len(seq)):
+                if seq[i] == 'A':
+                    mask[0] |= (1<<i)
+                if seq[i] == 'C':
+                    mask[1] |= (1<<i)
+                if seq[i] == 'T':
+                    mask[2]  |= (1<<i)
+                if seq[i] == 'G':
+                    mask[3] |= (1<<i)
+                    
+            #print(mask)
+            return tuple(mask)
+
         for i in range(len(s)):
             if j <= len(s):
                 temp = s[i:i+j]
-                if seen[temp] == 1:
+                mask = getMask(temp)
+                
+                if seen[mask] == 1:
                     ans.append(temp)
-                    
-                seen[temp] += 1
+                
+                seen[mask] += 1
             
         return ans
                 
