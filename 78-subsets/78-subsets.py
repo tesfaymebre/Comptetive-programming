62ceea73a5ed33b8f6cb6e0eb2c05c0a987e0e36
tesfaymebre <95352714+tesfaymebre@users.noreply.github.com>
2022-05-13 18:15:48 +0300

    Time: 50 ms (42.30%), Space: 14.1 MB (81.39%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,14 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
+        nth_bit = 1 << n
         ans = []
         
         for i in range(2**n):
-            x = '{0:010b}'. format(i)[::-1]
+            bit = bin(i | nth_bit)[3:] 
             temp = []
             
             for j in range(n):
-                if int(x[j]):
+                if int(bit[j]):
                     temp.append(nums[j])
             ans.append(temp)
         
