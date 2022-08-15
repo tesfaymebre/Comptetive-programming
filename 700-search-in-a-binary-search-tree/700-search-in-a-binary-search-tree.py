cf16a09c710ec1af45d7ff340e3224bd16712692
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-08-15 11:05:05 +0300

    Time: 122 ms (46.78%), Space: 16.6 MB (24.65%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.py
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.py
@@ -6,13 +6,10 @@
 #         self.right = right
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        curr = root
-        while curr:
-            if curr.val > val:
-                curr = curr.left
-            elif curr.val < val:
-                curr = curr.right
-            else:
-                return curr
-        return None
-            
+        if not root:
+            return
+        
+        if root.val == val:
+            return root
+        
+        return self.searchBST(root.left,val) if root.val > val else self.searchBST(root.right,val)
