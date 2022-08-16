1b02f6aed2aeb2dbe7140ccd79a4098e0b15c92e
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-08-16 17:36:30 +0300

    Time: 68 ms (37.39%), Space: 16.8 MB (5.10%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -17,24 +17,29 @@ class Solution:
             curr = curr.children[l]
         
         curr.isWord = True
+    
     @cache
     def search(self,s):
+        # if s in cache:
+        #     return cache[s]
+        
         curr = self.root
         found = False
         for indx,l in enumerate(s):
             if curr.isWord:
                 found |= self.search(s[indx:])
-                
             if l not in curr.children:
-                return False or found
-
+                # cache[s] = found
+                return found
             curr = curr.children[l]
-            if found: return found
+            
+        # cache[s] =
         return found or curr.isWord
+        # return cache[s]
         
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         for word in wordDict:
             self.insert(word)
         
         return self.search(s)
-        
+         
