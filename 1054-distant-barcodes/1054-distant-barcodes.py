e1dce6bed8f14d7cae79ed6cebe81e1ea7e85d3c
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-10-02 21:26:34 +0300

    Time: 1278 ms (10.92%), Space: 16.6 MB (15.79%) - LeetHub

--- 1054-distant-barcodes/1054-distant-barcodes.py
+++ 1054-distant-barcodes/1054-distant-barcodes.py
@@ -1,12 +1,12 @@
 class Solution:
     def rearrangeBarcodes(self, barcodes: List[int]) -> List[int]:
-        # Our ans to return
+        # ans to return
         ans = []
 
-        # Get the freq
+        # Get the frequency of number in the list barcodes
         freq = collections.Counter(barcodes)
 
-        # Create a max-heap based on frequency
+        # Create a max-heap based on frequency and notice the "- val" in order to make max heap. by         #default heapify works for min heap. so in order to have max heap we should negate the               #value.
         heap = [[-val, key] for key, val in freq.items()]
 
         # Heapify the heap
@@ -17,21 +17,20 @@ class Solution:
 
         while heap:
             ans.append(item[1])
-            item[0] += 1 # "Decrease" the count (remember our count is negative for min-heap, that's why we add)
-
-            """
-            'heapreplace' will pop the next item onto the heap, then push the old item!
-            Its the secret weapon for this solution.
-            We heappop if we don't want to push 'item' back onto the heap.
-            """
+            
+            # "Decrease" the frequency (remember our frequency is negative, that's why we add)
+            item[0] += 1 
+            
+            #hold the next varcode which is not equal with its previous barcode
             temp = heapq.heappop(heap)
             
+            #push item back to heap if item[1]s count is not equal to zero
             if item[0] < 0:
                 heapq.heappush(heap,item)
-            # item = heapq.heapreplace(heap, [item[0], item[1]]) if item[0] < 0 else heapq.heappop(heap)
+           
             item = temp
 
-        # Because I do a `heappop` outside of the while loop, we should append the last element!
+        # One item will remain outside of the while loop, we should append the last element!
         ans.append(item[1])
 
         return ans
