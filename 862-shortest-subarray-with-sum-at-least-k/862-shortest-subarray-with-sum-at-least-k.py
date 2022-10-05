651e6057d07f635d5b62f7be55a41ec11dacf7b5
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-10-05 14:05:35 +0300

    Time: 2217 ms (50.64%), Space: 28.7 MB (25.42%) - LeetHub

--- 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.py
+++ 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.py
@@ -1,34 +1,19 @@
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
+        que = deque([[0,0]])
+        cur_sum = 0
+        left = 0
+        ans = len(nums) + 1
         
-        if len(nums) == 0:
-            return -1
+        for i in range(len(nums)):
+            cur_sum += nums[i]
             
-        total = 0
-        que = deque()
-        min_len = len(nums) + 1
-
-        for val in enumerate(nums):
-
-            while que and val[1] <= 0:
-                ptr = que.pop()
-                val = (ptr[0], ptr[1] + val[1])
-                total -= ptr[1]
-
-            if val[1] <= 0:
-                total = 0
-                que.clear()
-            else:
-                total += val[1]
-                que.append(val)
-
-                while total >= k:
-                    ptr = que.popleft()
-                    min_len = min(min_len, val[0] + 1 - ptr[0])
-                    total -= ptr[1]
-                    
-        return min_len if min_len <= len(nums) else -1
-                    
+            while que and cur_sum - que[0][1] >= k:
+                ans = min(ans,i - que.popleft()[0] + 1)
                 
+            while que and que[-1][1] > cur_sum:
+                que.pop()
+                
+            que.append([i+1,cur_sum])
             
-            
+        return ans if ans != len(nums) + 1 else -1
