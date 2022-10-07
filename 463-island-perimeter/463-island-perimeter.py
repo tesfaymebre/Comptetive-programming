5e5d45086c1fcc1a183bf30b7429f55bbbc7b24c
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-10-07 14:12:56 +0300

    Time: 959 ms (57.46%), Space: 21.8 MB (16.79%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.py
+++ 463-island-perimeter/463-island-perimeter.py
@@ -1,30 +1,28 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         
-        def dfs(i,j):
-            if (i,j) in self.visited:
-                return
+        def dfs(i,j,visited):
+            visited.add((i,j))
             
-            self.visited.add((i,j))
-                
+            perimeter = 0
             for x,y in DIR:
                 r,c = i+x, j+y
                 
                 if not in_bound(r,c) or grid[r][c] == 0:
-                    self.perimeter += 1
-                else:
-                    dfs(r, c)
+                    perimeter += 1
+                elif (r,c) not in visited:
+                    perimeter += dfs(r, c,visited)
+                    
+            return perimeter
                 
-        self.visited = set()
-        self.perimeter = 0
         r,c = len(grid),len(grid[0])
         in_bound = lambda i,j: 0 <= i < r and 0 <= j < c
         DIR = [[0,1],[1,0],[-1,0],[0,-1]]
         
+        visited = set()
         for i in range(r):
             for j in range(c):
                 if grid[i][j] == 1:
-                    dfs(i,j)
-                    break
+                    return dfs(i,j,visited)
                     
-        return self.perimeter
+        return -1
