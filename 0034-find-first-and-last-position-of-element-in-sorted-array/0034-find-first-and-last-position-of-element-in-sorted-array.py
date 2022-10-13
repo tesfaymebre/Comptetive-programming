55416b9ea2dc04c9e888808464ce2c05c0ce39f5
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-10-13 13:59:15 +0300

    Time: 175 ms (43.18%), Space: 15.5 MB (49.43%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,30 +1,20 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        ans = [-1,-1]
+        def BST_left(arr,t):
+            left,right = 0,len(arr) - 1
+            while left <= right:
+                mid = left + (right - left)//2
+                if arr[mid] < target: left = mid + 1
+                else: right = mid - 1      
+            return left
         
-        if not nums: return ans
-        
-        left,right = 0,len(nums) - 1
-        
-        while left <= right:
-            mid = left + (right - left)//2
-            
-            if nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
+        def BST_right(arr,t):
+            left,right = 0,len(arr)-1
+            while left <= right:
+                mid = left + (right - left)//2
+                if arr[mid] > target: right = mid - 1
+                else: left = mid + 1   
+            return left
                 
-        if left < len(nums) and nums[left] == target: ans[0] = left
-        
-        left,right = 0,len(nums)-1
-        while left <= right:
-            mid = left + (right - left)//2
-            
-            if nums[mid] > target:
-                right = mid - 1
-            else:
-                left = mid + 1
-                
-        if nums[left-1] == target: ans[1] = left-1
-                
-        return ans
+        left,right = BST_left(nums,target),BST_right(nums,target)-1
+        return [left,right] if left <= right else [-1,-1]
