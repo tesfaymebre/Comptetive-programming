efcc114bbd6dbe96fec8817155379bcbf97d9eab
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-10-18 16:36:28 +0300

    Time: 67 ms (22.43%), Space: 13.8 MB (98.94%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -1,79 +1,17 @@
 class Solution:
-    def fill_right(self,start_state,matrix):
-        curr_value = start_state[2]
-        n = len(matrix)
-        
-        for i in range(start_state[1], n):
-            curr_value += 1
-            if matrix[start_state[0]][i] == 0:
-                matrix[start_state[0]][i] = curr_value
-                start_state[1] += 1
-            else:
-                break
-                
-            start_state[2] = curr_value
-        
-        start_state[0] += 1
-        start_state[1] -= 1
-            
-    def fill_down(self,start_state,matrix):
-        n = len(matrix)
-        curr_value = start_state[2]
-        
-        for i in range(start_state[0],n):
-            curr_value += 1
-            print(start_state)
-            if matrix[i][start_state[1]] == 0:
-                matrix[i][start_state[1]] = curr_value
-                start_state[0] += 1
-            else:
-                break
-                
-            start_state[2] = curr_value 
-        
-        start_state[0] -= 1
-        start_state[1] -= 1
-        
-    def fill_left(self,start_state,matrix):
-        n = len(matrix)
-        curr_value = start_state[2]
-        
-        for i in range(start_state[1],-1,-1):
-            curr_value += 1
-            if matrix[start_state[0]][i] == 0:
-                matrix[start_state[0]][i] = curr_value
-                start_state[1] -= 1
-            else:
-                break
-                
-            start_state[2] = curr_value
-        
-        start_state[0] -= 1
-        start_state[1] += 1
-            
-    def fill_up(self,start_state,matrix):
-        n = len(matrix)
-        curr_value = start_state[2]
-        
-        for i in range(start_state[0],-1,-1):
-            curr_value += 1
-            if matrix[i][start_state[1]] == 0:
-                matrix[i][start_state[1]] = curr_value
-                start_state[0] -= 1
-            else:
-                break
-                
-            start_state[2] = curr_value 
-        
-        start_state[0] += 1
-        start_state[1] += 1
-        
     def generateMatrix(self, n: int) -> List[List[int]]:
-        matrix = [[0]*n for _ in range(n)]
-        start_state = [0,0,0]
-        for _ in range(n):
-            self.fill_right(start_state,matrix)
-            self.fill_down(start_state,matrix)
-            self.fill_left(start_state,matrix)
-            self.fill_up(start_state,matrix)  
+        matrix = [[0] * n for _ in range(n)]
+        col = 0
+        row = 0
+        col_change = 1
+        row_change = 0
+        in_bound = lambda r,c: 0 <= r < n and 0 <= c < n
+        
+        for i in range(n*n):
+            matrix[row][col] = i + 1
+            new_row = row+row_change
+            new_col = col+col_change
+            if not in_bound(new_row,new_col) or matrix[row+row_change][col+col_change] != 0:
+                col_change, row_change = -row_change, col_change
+            col, row = col + col_change, row + row_change
         return matrix
