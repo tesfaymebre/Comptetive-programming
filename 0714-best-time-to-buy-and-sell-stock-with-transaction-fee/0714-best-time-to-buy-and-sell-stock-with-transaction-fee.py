4096914d291856e5c4ce9d1caafb43fe6b3207b8
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-10-23 14:32:19 +0300

    Time: 2484 ms (20.80%), Space: 21.4 MB (54.12%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,23 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         #bottom up solution
-        dp = [[0,0] for _ in range(len(prices)+1)]
+        dp = [0,0]
         
         for pos in range(len(prices)-1,-1,-1):
+            dp_old = dp
+            
             for bought in [True,False]:
                 
                 max_profit = 0
                 
                 if not bought:
-                    max_profit = max(max_profit,dp[pos+1][True]-prices[pos])
+                    max_profit = max(max_profit,dp_old[True]-prices[pos])
                 else:
-                    max_profit = max(max_profit,dp[pos+1][False] + prices[pos] - fee)
+                    max_profit = max(max_profit,dp_old[False] + prices[pos] - fee)
                     
-                max_profit = max(max_profit,dp[pos+1][bought])
+                max_profit = max(max_profit,dp_old[bought])
                 
-                dp[pos][bought] = max_profit
+                dp[bought] = max_profit
                 
-        return dp[0][False]
+        return dp[False]
         
         #top down solution
 #         memo = {}
