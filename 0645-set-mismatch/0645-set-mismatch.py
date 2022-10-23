e614a4b718e6b8de94ca277def2c2bbc9133d76d
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-10-23 13:15:08 +0300

    Time: 470 ms (42.53%), Space: 15.8 MB (60.69%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        items = Counter(nums)
+        for i in range(1,len(nums)+1):
+            if items[i] == 0:
+                return [(items.most_common(1))[0][0] , i]
