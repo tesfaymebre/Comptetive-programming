cf26507079b294b57ef87819b5025913d06d4232
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-10-23 13:49:30 +0300

    Time: 2861 ms (14.24%), Space: 23.2 MB (29.23%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,25 +1,45 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        memo = {}
+        #bottom up solution
+        dp = [[0,0] for _ in range(len(prices)+1)]
         
-        def dp(pos:int,bought:bool) -> int:
-            if pos == len(prices):
-                return 0
-            
-            if (pos,bought) in memo:
-                return memo[(pos,bought)]
-            
-            max_profit = 0
-            
-            if not bought:
-                max_profit = max(max_profit,dp(pos+1,True) - prices[pos])
-            else:
-                max_profit = max(max_profit,dp(pos+1,False) + prices[pos] - fee)
+        for pos in range(len(prices)-1,-1,-1):
+            for bought in [True,False]:
                 
-            max_profit = max(max_profit,dp(pos+1,bought))
-            
-            memo[(pos,bought)] = max_profit
-            return memo[(pos,bought)]
+                max_profit = 0
+                
+                if not bought:
+                    max_profit = max(max_profit,dp[pos+1][True]-prices[pos])
+                else:
+                    max_profit = max(max_profit,dp[pos+1][False] + prices[pos] - fee)
+                    
+                max_profit = max(max_profit,dp[pos+1][bought])
+                
+                dp[pos][bought] = max_profit
+                
+        return dp[0][False]
         
-        return dp(0,False)
+        #top down solution
+#         memo = {}
+        
+#         def dp(pos:int,bought:bool) -> int:
+#             if pos == len(prices):
+#                 return 0
+            
+#             if (pos,bought) in memo:
+#                 return memo[(pos,bought)]
+            
+#             max_profit = 0
+            
+#             if not bought:
+#                 max_profit = max(max_profit,dp(pos+1,True) - prices[pos])
+#             else:
+#                 max_profit = max(max_profit,dp(pos+1,False) + prices[pos] - fee)
+                
+#             max_profit = max(max_profit,dp(pos+1,bought))
+            
+#             memo[(pos,bought)] = max_profit
+#             return memo[(pos,bought)]
+        
+#         return dp(0,False)
         
