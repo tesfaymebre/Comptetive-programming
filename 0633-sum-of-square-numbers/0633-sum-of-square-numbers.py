10bf1828cb20df6271b861906085cb1c477b172f
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-10-23 22:29:00 +0300

    Time: 258 ms (78.60%), Space: 13.8 MB (64.13%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        left = 0
+        right = int(sqrt(c))
+        while left <= right:
+            cur = left * left + right * right
+            if cur == c: return True
+            if cur < c:
+                left += 1
+            else:
+                right -= 1
+        return False
+        
