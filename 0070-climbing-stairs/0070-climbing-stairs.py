a93cab18827c49d27b375bdb753508ca004a17e4
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-10-24 13:51:20 +0300

    Time: 49 ms (63.75%), Space: 14 MB (11.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,17 +1,20 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+       
+    #top down dp solution
         memo = {}
         
         def dp(pos):
-            if pos == n:
+            if pos == 1:
                 return 1
-            elif pos > n:
-                return 0
+            elif pos == 2:
+                return 2
             
             if pos in memo:
-                return memo[pos
-                           ]
-            memo[pos] = dp(pos+1) + dp(pos+2)
+                return memo[pos]
+            
+            memo[pos] = dp(pos-1) + dp(pos-2)
             return memo[pos]
         
-        return dp(0)
+        return dp(n)
+    
