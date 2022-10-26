3337aa4166c4a1d34939612ec0b1071e5669614a
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-10-26 13:57:47 +0300

    Time: 112 ms (60.81%), Space: 14.1 MB (20.21%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,5 +1,17 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        #bottom up space optimized dp solution
+        first = 0
+        second = 0
+        
+        for i in range(len(cost)-1,-1,-1):
+            curr = cost[i] + min(first,second)
+            second = first
+            first = curr
+            
+        return min(first,second)
+    
+        """
         #bottom up dp solution
         dp = [0]*(len(cost)+2)
         
@@ -7,6 +19,8 @@ class Solution:
             dp[i] = cost[i] + min(dp[i+1],dp[i+2])
         
         return min(dp[0],dp[1])
+        """
+        
     
         """
         #top down dp solution
