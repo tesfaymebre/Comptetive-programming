d5ed44391413b0fb69c82b857aefdc4002ee7e64
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-10-26 14:21:35 +0300

    Time: 90 ms (99.58%), Space: 15.8 MB (54.48%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,36 +1,36 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         #bottom up dp solution
-       
-        dp = [[grid[0][0]]*(len(grid[0])) for _ in range(len(grid))]
+        row = len(grid)
+        col = len(grid[0])
         
-        for r in range(1,len(grid)):
-            dp[r][0] = grid[r][0]+dp[r-1][0]
-            
-        for c in range(1,len(grid[0])):
-            dp[0][c] = grid[0][c] + dp[0][c-1]
+        dp = [[float('inf')]*(col+1) for _ in range(row+1)]
+        dp[row-1][col] = 0
+        dp[row][col-1] = 0
         
-        for r in range(1,len(grid)):
-            for c in range(1,len(grid[0])):
-                dp[r][c] = grid[r][c] + min(dp[r][c-1],dp[r-1][c])
+        for r in range(row-1,-1,-1):
+            for c in range(col-1,-1,-1):
+                dp[r][c] = grid[r][c] + min(dp[r][c+1],dp[r+1][c])
+                
+        return dp[0][0]
         
-        return dp[-1][-1]
-        
+        """
         #top down dp solution
-#         in_bound = lambda r, c: 0 <= r < len(grid) and 0 <= c < len(grid[0])
-#         memo = {}
+        in_bound = lambda r, c: 0 <= r < len(grid) and 0 <= c < len(grid[0])
+        memo = {}
         
-#         def dp(r,c):
-#             if (r,c) == (len(grid)-1, len(grid[0])-1):
-#                 return grid[-1][-1]
+        def dp(r,c):
+            if not in_bound(r, c):
+                return float('inf')
+                
+            if (r,c) == (len(grid)-1, len(grid[0])-1):
+                return grid[-1][-1]
             
-#             if (r,c) in memo:
-#                 return memo[(r,c)]
+            if (r,c) in memo:
+                return memo[(r,c)]
 
-#             if not in_bound(r, c):
-#                 return float('inf')
-            
-#             memo[(r,c)] = grid[r][c] + min(dp(r,c+1),dp(r+1,c))      
-#             return memo[(r,c)]
+            memo[(r,c)] = grid[r][c] + min(dp(r,c+1),dp(r+1,c))      
+            return memo[(r,c)]
         
-#         return dp(0,0)
+        return dp(0,0)
+        """
