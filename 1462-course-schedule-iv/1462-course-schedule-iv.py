b80d3abd4839e5f2dd1121a6ef92f4322689e612
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-10-27 12:34:41 +0300

    Time: 1703 ms (65.21%), Space: 17.5 MB (93.26%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,5 +1,35 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        #using topological sort
+        graph = defaultdict(list)
+        indegree = defaultdict(int)
+        parent = defaultdict(set)
+        
+        for pre,post in prerequisites:
+            graph[pre].append(post)
+            parent[post].add(pre)
+            indegree[post] += 1
+            
+        queue = deque()
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                queue.append(course)
+                
+        while queue:
+            pre = queue.popleft()
+
+            for post in graph[pre]:
+                parent[post].update(parent[pre])
+                indegree[post] -= 1
+
+                if indegree[post] == 0:
+                    queue.append(post)
+       
+        return [u in parent[v] for u,v in queries]
+                    
+        
+        """
+        #using floyd warshall algorithm
         dp = [[False]*numCourses for _ in range(numCourses)]
         
         for pre,post in prerequisites:
@@ -8,9 +38,10 @@ class Solution:
         for k in range(numCourses):
             for j in range(numCourses):
                 for i in range(numCourses):
-                    if i == k or j == k:
-                        continue
+                    # if i == k or j == k:
+                    #     continue
                     
                     dp[j][i] = dp[j][i] or (dp[j][k] and dp[k][i])
                     
         return [dp[u][v] for u,v in queries]
+        """
