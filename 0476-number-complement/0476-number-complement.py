a0ec66aa1f0918f61b5dd9ec683e242ae0a60b23
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-11-02 11:50:18 +0300

    Time: 72 ms (5.32%), Space: 13.9 MB (9.52%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,5 +1,19 @@
 class Solution:
     def findComplement(self, num: int) -> int:
+        #solution 2 with out using strig
+        complement = 0
+        shift_count = 0
+        
+        while num:
+            if num&1 == 0:
+                complement |= 1<<shift_count
+            
+            num = num >> 1
+            shift_count += 1
+            
+        return complement
+        """
+        #solution 1
         complement = ""
         
         while num:
@@ -12,3 +26,4 @@ class Solution:
                 complement = '1'+complement
                 
         return int(complement,2)
+        """
