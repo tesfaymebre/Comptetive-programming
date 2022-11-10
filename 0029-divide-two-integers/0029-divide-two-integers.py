32d2d001914482ab43b23875abf3a5f27c0a3a36
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-11-10 22:29:19 +0300

    Time: 74 ms (17.91%), Space: 14 MB (27.54%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,15 +1,22 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        positive = (dividend < 0) is (divisor < 0)
+        positive = False
+        if (dividend < 0) and (divisor < 0) or ((dividend >= 0) and (divisor > 0)):
+            positive = True
+            
         dividend, divisor = abs(dividend), abs(divisor)
         res = 0
+        
         while dividend >= divisor:
             temp, i = divisor, 1
+            
             while dividend >= temp:
                 dividend -= temp
                 res += i
                 i <<= 1
                 temp <<= 1
+                
         if not positive:
             res = -res
+            
         return min(max(-2147483648, res), 2147483647)
