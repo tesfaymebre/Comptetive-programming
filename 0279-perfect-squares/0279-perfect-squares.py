9d61ab8cfda09d0d19b64bb0beb88a1679e70479
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-11-11 16:01:05 +0300

    Time: 2150 ms (56.43%), Space: 198.7 MB (5.01%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,8 +1,35 @@
 class Solution:
     def numSquares(self, n: int) -> int:
+        #bfs solution
+        
+        squares = [i*i for i in range(1,floor(sqrt(n))+1)]
+        queue = deque([n])
+        depth = 1
+        
+        while queue:
+            size_queue = len(queue)
+            
+            for j in range(size_queue):
+                curr = queue.popleft()
+                
+                for square in squares:
+                    if curr == square:
+                        return depth
+                    if curr < square:
+                        break
+                        
+                    queue.append(curr-square)
+                    
+            depth += 1
+            
+        return depth
+        
+        """
+        # bottom up dp solution
         dp = [0] + [float('inf')]*n
         
         for i in range(1,n+1):
             dp[i] = min(dp[i-j*j] for j in range(1,floor(sqrt(i))+1)) + 1
             
         return dp[n]
+        """
