45812cec9155e4fc88c0122ff218c478cd6fc1de
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-11-17 22:49:20 +0300

    Time: 581 ms (33.11%), Space: 17.7 MB (96.42%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,5 +1,60 @@
+class DSU:
+    def __init__(self,size):
+        self.parent = [i for i in range(size)]
+        self.rank = [1]*size
+        
+    def find(self,name_idx):
+        if self.parent[name_idx] != name_idx:
+            self.parent[name_idx] = self.find(self.parent[name_idx])
+            
+        return self.parent[name_idx]
+        
+    def union(self,a,b):
+        root_a = self.find(a)
+        root_b = self.find(b)
+        
+        if self.rank[root_a] < self.rank[root_b]:
+            root_a,root_b = root_b,root_a
+            
+        self.parent[root_b] = root_a
+        self.rank[root_a] += self.rank[root_b]
+        
+        return self.parent[root_a]
+        
 class Solution:
+    
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        #union find solution
+        
+        size = len(accounts)
+        union_find = DSU(size)
+        
+        email_to_name_idx = {}
+        
+        for name_idx in range(size):
+            for email in accounts[name_idx][1:]:
+                if email in email_to_name_idx:
+                    union_find.union(email_to_name_idx[email],name_idx)
+                
+                email_to_name_idx[email] = union_find.parent[name_idx]
+                
+        result = defaultdict(list)
+        
+        for email,name_idx in email_to_name_idx.items():
+            root_name_idx = union_find.find(name_idx)
+            result[root_name_idx].append(email)
+            
+            
+        ans = []
+        
+        for name_idx,emails in result.items():
+            ans.append([accounts[name_idx][0]] + sorted(emails))
+            
+        return ans
+    
+    """
+        # dfs solution
+        
         graph = defaultdict(list)
         
         
@@ -33,3 +88,6 @@ class Solution:
                 merged_accounts.append([accounts[i][0]]+ sorted(emails))
                 
         return merged_accounts
+        """
+                
+       
