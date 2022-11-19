2d7ac5850b3b0b326bf18142d478ea78e668995a
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-11-19 22:09:34 +0300

    Time: 585 ms (56.63%), Space: 14.8 MB (57.83%) - LeetHub

---
+++ 0587-erect-the-fence/0587-erect-the-fence.py
@@ -0,0 +1,28 @@
+class Solution:
+    def outerTrees(self, points: List[List[int]]) -> List[List[int]]:
+        
+        def crossProduct(p1,p2,p3):
+            
+            a = p3[0]-p2[0]
+            b = p3[1]-p2[1]
+            c = p2[0]-p1[0]
+            d = p2[1]-p1[1]
+            
+            return  a*d - b*c
+        
+        def constructHalfHull(points):
+            stack = []
+            for p in points:
+    
+                while len(stack)>=2 and crossProduct(stack[-2],stack[-1],p)>0:
+                    stack.pop()
+               
+                stack.append(tuple(p))
+            return stack
+        
+        points.sort()
+        leftToRight = constructHalfHull(points)
+        
+        rightToLeft = constructHalfHull(points[::-1])
+        
+        return list(set(leftToRight + rightToLeft))
