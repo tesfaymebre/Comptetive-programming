ec4c6ee9553d4fb6d1dd106ab057469439f5b685
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-11-21 22:59:59 +0300

    Time: 2003 ms (48.81%), Space: 14.4 MB (97.75%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -0,0 +1,31 @@
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        DIR = [[0,1],[1,0],[0,-1],[-1,0]]
+        inbound = lambda r,c: -1 < r < len(maze) and -1 < c < len(maze[0])
+        
+        
+        rows, cols = len(maze), len(maze[0])
+        dirs = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        
+        start_row, start_col = entrance
+        maze[start_row][start_col] = "+"
+        
+        queue = deque()
+        queue.append([start_row, start_col, 0])
+        
+        while queue:
+            curr_row, curr_col, curr_distance = queue.popleft()
+            
+            for d in dirs:
+                next_row = curr_row + d[0]
+                next_col = curr_col + d[1]
+                
+                if inbound(next_row,next_col) and maze[next_row][next_col] == ".":
+                    
+                    if 0 == next_row or next_row == rows - 1 or 0 == next_col or next_col == cols - 1:
+                        return curr_distance + 1
+                  
+                    maze[next_row][next_col] = "+"
+                    queue.append([next_row, next_col, curr_distance + 1])
+            
+        return -1
