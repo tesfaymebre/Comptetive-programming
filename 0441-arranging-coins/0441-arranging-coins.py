a8cc2ad005e2108669ee6fbf798f19d2e2bef4ca
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-11-25 15:54:16 +0300

    Time: 942 ms (38.54%), Space: 13.9 MB (12.57%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -2,13 +2,10 @@ class Solution:
     def arrangeCoins(self, n: int) -> int:
         curr = 0
         
-        for i in range(n+1):
+        for i in range(n+2):
             curr += i
             
             if curr > n:
                 return i-1
-            
-            if curr == n:
-                return i
             
         
