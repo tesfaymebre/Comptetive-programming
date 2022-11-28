bac39755784c3c4976c991d2583897531a4a60c2
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-11-28 14:37:49 +0300

    Time: 1989 ms (9.06%), Space: 14.7 MB (17.87%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -1,21 +1,21 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        #bottom up solution
-        nums.sort()
-        dp = [[num] for num in nums]
-        idx_max = 0
+#         #bottom up solution
+#         nums.sort()
+#         dp = [[num] for num in nums]
+#         idx_max = 0
         
-        for i in range(len(nums)):
-            for j in range(i):
-                if (nums[i] % nums[j] == 0) and len(dp[i]) < len(dp[j])+1:
-                    dp[i] = dp[j] + [nums[i]]
+#         for i in range(len(nums)):
+#             for j in range(i):
+#                 if (nums[i] % nums[j] == 0) and len(dp[i]) < len(dp[j])+1:
+#                     dp[i] = dp[j] + [nums[i]]
                     
-            if len(dp[idx_max]) < len(dp[i]):
-                idx_max = i
+#             if len(dp[idx_max]) < len(dp[i]):
+#                 idx_max = i
         
-        return dp[idx_max]
+#         return dp[idx_max]
+        
     
-        """
         #top down solution
         seen = {}
         
@@ -23,20 +23,18 @@ class Solution:
             if len(self.res) < len(path):
                 self.res = path[:]
             
-            for i in range(idx+1,len(nums)):
-                if nums[i] % nums[idx] == 0:
+            for i in range(idx,len(nums)):
+                if not path or nums[i] % path[-1] == 0:
                     if nums[i] not in seen or seen[nums[i]] < len(path):
                         seen[nums[i]] = len(path)
-                        helper(i,path + [nums[i]])
+                        helper(i+1,path + [nums[i]])
                     
             return
         
         nums.sort()
         self.res = []
-        
-        for i in range(len(nums)):
-            helper(i,[nums[i]])
+
+        helper(0,[])
        
         return self.res
-        """
             
