e47972ee1120e74be07114dc9cade24b3cff90cd
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-11-30 19:19:08 +0300

    Time: 189 ms (75.50%), Space: 14.3 MB (45.64%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,16 +1,45 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        #binary search solution
+        accumulate = [nums[0]]
+        
+        def BS(num):
+            left = 0
+            right = len(accumulate)-1
+
+            while left <= right:
+                mid = left + (right-left)//2
+                
+                if accumulate[mid] < num:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+                    
+            return left
+        
+        for num in nums:
+            pos = BS(num)
+            
+            if pos == len(accumulate):
+                accumulate.append(num)
+            else:
+                accumulate[pos] = num
+                
+        return len(accumulate)
+    
+    
+        """
         #bottom up
         dp = [1]*len(nums)
-        maxi = 1
         
         for i in range(len(nums)):
             for j in range(i):
                 if nums[i] > nums[j]:
                     dp[i] = max(dp[i],1+dp[j])
-                    maxi = max(maxi,dp[i])
                     
-        return maxi
+        return max(dp)
+        """
+        
         """
         #top down
         @cache
