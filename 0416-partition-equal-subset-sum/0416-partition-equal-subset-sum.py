b87bc8234023597125f5007f92464fdb71ed3bbc
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-01 14:15:31 +0300

    Time: 8659 ms (5.35%), Space: 642.3 MB (5.00%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,14 +1,16 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        
         @cache
         def helper(idx,sub1):
-            if idx == len(nums) or sub1 == total - sub1:
-                return sub1 == total - sub1
+            if sub1 == total - sub1:
+                return True
             
-            if sub1 > total - sub1:
+            if idx == len(nums) or sub1 > total - sub1:
                 return False
             
             return helper(idx+1,sub1+nums[idx]) or helper(idx+1,sub1)
         
         total = sum(nums)
         return helper(0,0)
+        
