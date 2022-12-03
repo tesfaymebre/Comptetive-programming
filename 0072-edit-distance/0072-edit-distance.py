2e3799b9102104d0098392e1ee04cfda82dcb84c
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-03 12:46:31 +0300

    Time: 492 ms (29.35%), Space: 14 MB (96.48%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,5 +1,36 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        #space optimized bottom up dp soltution
+        size1 = len(word1)
+        size2 = len(word2)
+        
+        if not word1:
+            return size2
+        elif not word2:
+            return size1
+        
+        dp = [[0]*(size2 + 1) for _ in range(2)]
+            
+        for idx1 in range(size1 - 1,-1,-1):
+            for idx2 in range(size2 - 1,-1,-1):
+                if idx2+1 == size2:
+                    dp[idx1 & 1][-1] = size1-idx1
+                
+                if idx1+1 == size1:
+                    dp[(idx1+1)& 1][idx2] += size2-idx2
+                    
+                if word1[idx1] == word2[idx2]:
+                    dp[idx1&1][idx2] = dp[(idx1+1)&1][idx2+1]
+                else:
+                    insert = dp[(idx1)&1][idx2+1]
+                    delete = dp[(idx1+1)&1][idx2]
+                    replace = dp[(idx1+1)&1][idx2+1]
+                    
+                    dp[(idx1)&1][idx2] = 1 + min(insert,delete,replace)
+                    
+        return dp[0][0]
+    
+        """
         #bottom up dp soltution
         size1 = len(word1)
         size2 = len(word2)
@@ -24,6 +55,7 @@ class Solution:
                     dp[idx1][idx2] = 1 + min(insert,delete,replace)
                     
         return dp[0][0]
+        """
         
         """
         #top down dp solution
