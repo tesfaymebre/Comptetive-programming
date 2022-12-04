ab25d6751f705f910addbe8d370da3c624408481
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-04 13:58:09 +0300

    Time: 6563 ms (7.05%), Space: 13.9 MB (89.82%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -1,6 +1,23 @@
 class Solution:
     def minCut(self, s: str) -> int:
+        #bottom up dp solution
+        size = len(s)
         
+        dp = [float('inf')]*(size)+[0]
+        
+        for left in range(size-1,-1,-1):
+            count = float('inf')
+            
+            for right in range(left,size):
+                if s[left:right+1] == s[left:right+1][::-1]:
+                    count = min(count,1+dp[right+1])
+                    
+            dp[left] = count
+            
+        return dp[0]-1
+        
+        """
+        #top down dp
         def helper(left):
             if left not in memo:
                 if left == len(s):
@@ -21,5 +38,6 @@ class Solution:
         
         return helper(0) - 1
         
+        """
                     
                 
