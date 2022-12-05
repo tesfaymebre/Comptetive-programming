fdb1a386f7e622edbe38d7534520d9ba9f3a5c6f
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-05 10:05:09 +0300

    Time: 653 ms (97.58%), Space: 32.1 MB (13.37%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,5 +1,25 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        #bottom up dp solution
+        size = len(s)
+        dp = [[] for _ in range(size+1)]
+        dp[size] = [[]]
+        
+        for left in range(size-1,-1,-1):
+            result = []
+            
+            for right in range(left,size):
+                if s[left:right+1] == s[left:right+1][::-1]:
+                    sub_partitions = dp[right+1]
+                    
+                    result += [[s[left:right+1]] + p for p in sub_partitions]
+                    
+            dp[left] = result[:]
+            
+        return dp[0]
+        
+        """
+        #top down dp solution
         
         def helper(left):
             if left not in memo:
@@ -21,3 +41,4 @@ class Solution:
         memo = {}
         
         return helper(0)
+        """
