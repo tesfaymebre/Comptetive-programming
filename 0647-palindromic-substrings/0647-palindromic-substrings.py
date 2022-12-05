823785154aee724bb435e85690e9912c10dbaffe
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-05 12:00:24 +0300

    Time: 133 ms (86.39%), Space: 13.8 MB (76.00%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,25 +1,14 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        memo = {}
-        
-        def is_palindrome(left,right):
-            if (left,right) not in memo:
-                if left > right:
-                    return True
-
-                if s[left] != s[right]:
-                    return False
-
-                memo[(left,right)] = is_palindrome(left+1,right-1)
-                
-            return memo[(left,right)]
-        
         count = 0
         
-        for left in range(len(s)):
-            for right in range(left,len(s)):
-                if is_palindrome(left,right):
+        for center in range(len(s)):
+            for right in [center,center+1]:
+                left = center
+                
+                while left >= 0 and right < len(s) and s[left] == s[right]:
                     count += 1
-                    
+                    left -= 1
+                    right += 1
                     
         return count
