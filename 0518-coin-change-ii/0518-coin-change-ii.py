52ce5dabb378c876bcfbbc9bd879b6da78644b1b
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-06 09:58:38 +0300

    Time: 388 ms (72.58%), Space: 23.7 MB (49.76%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,8 +1,25 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        def helper(pos,amount):
-            if (pos,amount) not in memo:
-                if amount == 0:
+        #bottom up dp solution
+        
+        size = len(coins)
+        dp = [[1]+[0]*amount for _ in range(size+1)]
+        
+        for pos in range(size-1,-1,-1):
+            for capacity in range(amount+1):
+                if capacity >= coins[pos]:
+                    dp[pos][capacity] = dp[pos][capacity-coins[pos]] + dp[pos+1][capacity]
+                else:
+                    dp[pos][capacity] = dp[pos+1][capacity]
+                    
+        return dp[0][amount]
+        
+        """
+        #top down dp solution with memoization
+        
+        def helper(pos,capacity):
+            if (pos,capacity) not in memo:
+                if capacity == 0:
                     return 1
 
                 if pos == len(coins):
@@ -10,14 +27,15 @@ class Solution:
 
                 pick = 0
 
-                if amount >= coins[pos]:
-                    pick += helper(pos,amount-coins[pos])
+                if capacity >= coins[pos]:
+                    pick += helper(pos,capacity-coins[pos])
 
-                not_pick = helper(pos+1,amount)
+                not_pick = helper(pos+1,capacity)
 
-                memo[(pos,amount)] = pick + not_pick   
+                memo[(pos,capacity)] = pick + not_pick   
                 
-            return memo[(pos,amount)]
+            return memo[(pos,capacity)]
         
         memo = {}
         return helper(0,amount)
+        """
