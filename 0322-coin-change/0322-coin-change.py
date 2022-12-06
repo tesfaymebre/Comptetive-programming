f30b34cf37fd3911718502e62d90dc219666655f
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-06 09:24:08 +0300

    Time: 1898 ms (77.44%), Space: 54.3 MB (5.02%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,5 +1,48 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        # space optimized top down dp solution
+        def helper(capacity):
+            if capacity not in memo:
+                if capacity == 0:
+                    return 0
+
+                count = float('inf')
+
+                for i in range(len(coins)):
+                    if capacity >= coins[i]:
+                        count = min(count,1 + helper(capacity-coins[i]))
+
+                memo[capacity] = count
+            
+            return memo[capacity]
+        
+        memo = {}
+        ans = helper(amount)
+        
+        return ans if ans != float('inf') else -1
+        
+        """
+        #bottom up dp solution
+        size = len(coins)
+        dp = [[0]+[float('inf')]*(amount) for _ in range(size+1)]
+        
+        for pos in range(size-1,-1,-1):
+            for capacity in range(amount+1):
+                pick = float('inf')
+                
+                if capacity >= coins[pos]:
+                    pick = 1 + dp[pos][capacity-coins[pos]]
+                    
+                not_pick = dp[pos+1][capacity]
+                
+                dp[pos][capacity] = min(pick,not_pick)
+                
+        ans = dp[0][amount]
+        return ans if ans != float('inf') else -1
+        """
+        
+        """
+        #top down dp solution
         
         def dp(pos,capacity):
             if (pos,capacity) not in memo:
@@ -23,3 +66,5 @@ class Solution:
         ans = dp(0,amount)
         
         return ans if ans != float('inf') else -1
+        """
+        
