7d7dd4fbfaac431f50a277df93b73673e3e28ecf
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-06 09:33:09 +0300

    Time: 3915 ms (38.05%), Space: 14.2 MB (62.19%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,5 +1,18 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        #space optimized bottom up dp solution
+        size = len(coins)
+        dp = [0]+[float('inf')]*amount
+        
+        for capacity in range(amount+1):
+            
+            for i in range(size):
+                if capacity >= coins[i]:
+                    dp[capacity] = min(dp[capacity],1 + dp[capacity-coins[i]])
+            
+        return dp[amount] if dp[amount] != float('inf') else -1
+        
+        """
         # space optimized top down dp solution
         def helper(capacity):
             if capacity not in memo:
@@ -20,6 +33,7 @@ class Solution:
         ans = helper(amount)
         
         return ans if ans != float('inf') else -1
+        """
         
         """
         #bottom up dp solution
