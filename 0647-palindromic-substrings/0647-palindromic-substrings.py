e470b859ed7300ed037d1f65c4645f9d1737facc
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-06 11:27:35 +0300

    Time: 1539 ms (6.77%), Space: 22 MB (21.01%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,6 +1,61 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
+        #bottom up dp solution
+        
+        size = len(s)
+        
+        dp = [[False]*(size+1) for _ in range(size+1)]
+        
+        for left in range(size-1,-1,-1):
+            for right in range(size):
+                if left > right:
+                    dp[left][right] = True
+                elif s[left] == s[right]:
+                    if right - left > 1:
+                        dp[left][right] = dp[left+1][right-1]
+                    else:
+                        dp[left][right] = True
+                        
         count = 0
+        
+        for left in range(len(s)):
+            for right in range(left,len(s)):
+                if dp[left][right]:
+                    count += 1  
+                    
+        return count
+        
+        """
+        #top down dp solution
+        memo = {}
+        
+        def is_palindrome(left,right):
+            if (left,right) not in memo:
+                if left > right:
+                    return True
+
+                if s[left] != s[right]:
+                    return False
+
+                memo[(left,right)] = is_palindrome(left+1,right-1)
+                
+            return memo[(left,right)]
+        
+        count = 0
+        
+        for left in range(len(s)):
+            for right in range(left,len(s)):
+                if is_palindrome(left,right):
+                    count += 1
+                    
+                    
+        return count
+        """
+        
+
+        """
+        # starting from center
+         count = 0
         
         for center in range(len(s)):
             for right in [center,center+1]:
@@ -12,3 +67,4 @@ class Solution:
                     right += 1
                     
         return count
+        """
