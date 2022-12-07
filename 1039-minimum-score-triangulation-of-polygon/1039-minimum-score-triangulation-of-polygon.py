d2bf1517f11cf42c48312d644a967f4ee5393b67
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-07 10:31:13 +0300

    Time: 353 ms (42.48%), Space: 14 MB (82.68%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.py
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.py
@@ -1,5 +1,21 @@
 class Solution:
     def minScoreTriangulation(self, values: List[int]) -> int:
+        #bottom up dp solution
+        size = len(values)
+        dp = [[float('inf')]*(size+1) for _ in range(size+1)]
+        
+        for i in range(size-1,-1,-1):
+            for j in range(size):
+                for k in range(i+1,j):
+                    dp[i][j] = min(dp[i][j],dp[i][k]+dp[k][j]+values[i]*values[k]*values[j])
+                    
+                if dp[i][j] == float('inf'):
+                    dp[i][j] = 0
+                    
+        return dp[0][size-1]
+        """
+        #top down dp solution
+    
         memo = {}
         def dp(i, j):
             if (i, j) not in memo:
@@ -15,3 +31,7 @@ class Solution:
             
             return memo[(i, j)]
         return dp(0, len(values) - 1)
+        
+        #time complexity: O(n^3)
+        # space complexity: O(n^2)
+        """
