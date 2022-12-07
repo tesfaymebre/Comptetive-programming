afad258a85903b1a2bd9a36ae4045c5a2b38f596
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-07 12:36:03 +0300

    Time: 1126 ms (5.12%), Space: 14 MB (31.32%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
@@ -1,5 +1,26 @@
 class Solution:
     def mctFromLeafValues(self, arr: List[int]) -> int:
+        #bottom up dp solution
+        
+        size = len(arr)
+        dp = [[float('inf')]*(size+1) for _ in range(size+1)]
+        
+        for i in range(size-1,-1,-1):
+            for j in range(size):
+                if j-i+1 == 2:
+                    dp[i][j] = arr[i]*arr[j]
+                elif j-i+1 < 2:
+                    dp[i][j] = 0
+                    
+                for k in range(i+1,j):
+                    left = dp[i][k]+dp[k+1][j]+max(arr[i:k+1])*max(arr[k+1:j+1])
+                    right = dp[i][k-1]+dp[k][j]+max(arr[i:k])*max(arr[k:j+1])
+                    dp[i][j] = min(dp[i][j],left,right)
+                    
+        return dp[0][size-1]
+    
+        """
+        #top down dp solution
         def dp(i,j):
             if (i,j) not in memo:
                 
@@ -22,3 +43,4 @@ class Solution:
         memo = {}
         
         return dp(0,len(arr)-1)
+        """
