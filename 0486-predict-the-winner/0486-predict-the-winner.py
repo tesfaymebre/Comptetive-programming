f292f609dcf8f544114ea2cc8c872c7910b6b04a
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-08 10:22:15 +0300

    Time: 43 ms (83.91%), Space: 14.1 MB (49.26%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,13 +1,20 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        def dp(i,j):
-            if i == j:
-                return nums[i]
-           
-            curr_score = max(nums[i] - dp(i+1,j),nums[j] - dp(i,j-1))
-            return curr_score
+        #top down dp solution
         
+        def dp(i,j):
+            if (i,j) not in memo:
+                if i == j:
+                    return nums[i]
+
+                curr_score = max(nums[i] - dp(i+1,j),nums[j] - dp(i,j-1))
+                memo[(i,j)] = curr_score
+                
+            return memo[(i,j)]
+        
+        memo = {}
         player_1_score = dp(0,len(nums)-1)
         
         return True if player_1_score >= 0 else False
+        
         
