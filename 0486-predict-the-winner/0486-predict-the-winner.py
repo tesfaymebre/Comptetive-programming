058e0e252233c8c2e53dd5901ff098bc2fa82b7b
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-08 10:55:38 +0300

    Time: 65 ms (61.11%), Space: 13.9 MB (87.96%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,32 +1,11 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        #space optimized bottom up dp solution
-        
-        size = len(nums)
-        dp = [[0]*(size+1) for _ in range(2)]
-        
-        
-        dp[0][0] = nums[0]
-            
-        for i in range(size-1,-1,-1):
-            dp[i&1][i&1] = nums[i]
-            
-            for j in range(i,size):
-                dp[i&1][j] = max(nums[i]-dp[(i+1)&1][j],nums[j]-dp[i&1][j-1])
-                
-        return True if dp[0][size-1] >= 0 else False
-    
-        #time complexity: O(n^2)
-        #space complexity: O(2n) = O(n)
-    
-        """
         #bottom up dp solution
         
         size = len(nums)
         dp = [[0]*(size+1) for _ in range(size+1)]
         
-        for i in range(size):
-            dp[i][i] = nums[i]
+        dp[0][0] = nums[0]
             
         for i in range(size-1,-1,-1):
             for j in range(i,size):
@@ -36,7 +15,7 @@ class Solution:
         
         #time complexity: O(n^2)
         #space complexity: O(n^2)
-        """
+
     
         """
         #top down dp solution
