1fc20abff6d59337fbc6e7b49de9d56267f67abc
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-08 10:29:13 +0300

    Time: 58 ms (72.36%), Space: 14 MB (64.46%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,5 +1,20 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
+        #bottom up dp solution
+        
+        size = len(nums)
+        dp = [[0]*(size+1) for _ in range(size+1)]
+        
+        for i in range(size):
+            dp[i][i] = nums[i]
+            
+        for i in range(size-1,-1,-1):
+            for j in range(i,size):
+                dp[i][j] = max(nums[i]-dp[i+1][j],nums[j]-dp[i][j-1])
+                
+        return True if dp[0][size-1] >= 0 else False
+    
+        """
         #top down dp solution
         
         def dp(i,j):
@@ -16,5 +31,6 @@ class Solution:
         player_1_score = dp(0,len(nums)-1)
         
         return True if player_1_score >= 0 else False
+        """
         
         
