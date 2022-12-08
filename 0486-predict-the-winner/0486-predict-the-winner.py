dcdadba1b4feb3807f4359a76e34aea7585728ff
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-08 10:53:26 +0300

    Time: 28 ms (99.80%), Space: 14 MB (64.46%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,5 +1,25 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
+        #space optimized bottom up dp solution
+        
+        size = len(nums)
+        dp = [[0]*(size+1) for _ in range(2)]
+        
+        
+        dp[0][0] = nums[0]
+            
+        for i in range(size-1,-1,-1):
+            dp[i&1][i&1] = nums[i]
+            
+            for j in range(i,size):
+                dp[i&1][j] = max(nums[i]-dp[(i+1)&1][j],nums[j]-dp[i&1][j-1])
+                
+        return True if dp[0][size-1] >= 0 else False
+    
+        #time complexity: O(n^2)
+        #space complexity: O(2n) = O(n)
+    
+        """
         #bottom up dp solution
         
         size = len(nums)
@@ -13,6 +33,10 @@ class Solution:
                 dp[i][j] = max(nums[i]-dp[i+1][j],nums[j]-dp[i][j-1])
                 
         return True if dp[0][size-1] >= 0 else False
+        
+        #time complexity: O(n^2)
+        #space complexity: O(n^2)
+        """
     
         """
         #top down dp solution
@@ -31,6 +55,9 @@ class Solution:
         player_1_score = dp(0,len(nums)-1)
         
         return True if player_1_score >= 0 else False
+        
+        #time complexity: O(n^2)
+        #space complexity: O(n^2)
         """
         
         
