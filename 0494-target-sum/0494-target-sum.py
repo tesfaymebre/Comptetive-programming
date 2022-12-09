20bb3549b7a870938db0da08599ae9a82e98111b
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-09 14:47:56 +0300

    Time: 1567 ms (7.37%), Space: 14.1 MB (88.34%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,5 +1,26 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        #space optimized bottom up dp soltution
+        
+        size = len(nums)
+        total = sum(nums)
+        
+        dp = [[0]*(2*total+max(nums)+1) for _ in range(2)]
+        dp[size&1][target+total] = 1
+        
+        for idx in range(size-1,-1,-1):
+            dp[idx&1] = [0]*(2*total+max(nums)+1)
+            
+            for curr_sum in range(-total,total+1):
+                dp[idx&1][curr_sum + total] += dp[(idx+1)&1][curr_sum-nums[idx]+total]
+                dp[idx&1][curr_sum + total] += dp[(idx+1)&1][curr_sum+nums[idx]+total]
+                
+        return dp[0][total]
+    
+        #time complexity: O(total*n) 
+        #space complexity: O(2*total) = O(total)
+    
+        """
         #bottom up dp soltution
         size = len(nums)
         total = sum(nums)
@@ -13,6 +34,11 @@ class Solution:
                 dp[idx][curr_sum + total] += dp[idx+1][curr_sum+nums[idx]+total]
                 
         return dp[0][total]
+        
+        #time complexity: O(2*total*n) = O(total*n) 
+                            where total = sum(nums) and n = len(nums)
+        #space complexity: O(total*n)
+        """
                 
         
         """
@@ -30,4 +56,9 @@ class Solution:
         memo = {}
         
         return dp(0,0)
+        
+        #time complexity: O(2*total*n) = O(total*n) 
+                            where total = sum(nums) and n = len(nums)
+        #space complexity: O(total*n)
+         
         """
