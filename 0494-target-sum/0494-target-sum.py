5762730fc7de3cb239021db6b42486710eed0e30
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-09 14:17:21 +0300

    Time: 1154 ms (15.34%), Space: 14.4 MB (68.62%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,5 +1,23 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        #bottom up dp soltution
+        size = len(nums)
+        total = sum(nums)
+        
+        dp = [[0]*(2*total+max(nums)+1) for _ in range(size+1)]
+        dp[size][target+total] = 1
+        
+        for idx in range(size-1,-1,-1):
+            for curr_sum in range(-total,total+1):
+                dp[idx][curr_sum + total] += dp[idx+1][curr_sum-nums[idx]+total]
+                dp[idx][curr_sum + total] += dp[idx+1][curr_sum+nums[idx]+total]
+                
+        return dp[0][total]
+                
+        
+        """
+        #top down dp solution
+        
         def dp(idx,total):
             if (idx,total) not in memo:
                 if idx == len(nums):
@@ -12,3 +30,4 @@ class Solution:
         memo = {}
         
         return dp(0,0)
+        """
