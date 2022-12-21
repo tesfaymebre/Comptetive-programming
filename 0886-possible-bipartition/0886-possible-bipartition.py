6a388e8653fed229db23577c75fcdcd58a850998
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-21 15:10:30 +0300

    Time: 923 ms (71.64%), Space: 19.9 MB (92.70%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,5 +1,49 @@
+class UnionFind:
+    def __init__(self,size):
+        self.parent = [i for i in range(size)]
+        self.rank = [0]*size
+        
+    def find(self,x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+            
+        return self.parent[x]
+    
+    def union(self,x,y):
+        par_x = self.find(x)
+        par_y = self.find(y)
+        
+        if par_x == par_y:
+            return
+        
+        if self.rank[par_x]  < self.rank[par_y]:
+            par_x,par_y = par_y,par_x
+            
+        self.parent[par_y] = par_x
+        self.rank[par_x] += self.rank[par_y]
+        return
+    
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        
+        for a,b in dislikes:
+            graph[a].append(b)
+            graph[b].append(a)
+            
+        DSU = UnionFind(n+1)
+        
+        for node in range(1,n+1):
+            for child in graph[node]:
+                if  DSU.find(node) == DSU.find(child):
+                    return False
+                
+                DSU.union(graph[node][0],child)
+        
+        return True
+        
+        """
+        #bfs approach
         
         def bfs(i):
             queue = deque()
@@ -37,3 +81,4 @@ class Solution:
                     return False
                 
         return True
+        """
