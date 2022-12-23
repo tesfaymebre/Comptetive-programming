1a12607432861600decce2fb137f4d051526b49f
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2022-12-23 11:21:31 +0300

    Time: 41 ms (94.67%), Space: 14.7 MB (46.18%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,5 +1,28 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        #bottom up dp solution
+        size = len(prices)
+        dp = [[0,0] for _ in range(size+2)]
+        
+        for idx in range(size-1,-1,-1):
+            for status in ['sell','buy']:
+                profit = 0
+                
+                if status == 'buy':
+                    buy = -1*prices[idx] + dp[idx+1][1]
+                    not_buy = dp[idx+1][0]
+                    profit += max(buy,not_buy)
+                    dp[idx][0] = profit
+                else:
+                    sell = prices[idx] + dp[idx+2][0]
+                    not_sell = dp[idx+1][1]
+                    profit += max(sell,not_sell)
+                    dp[idx][1] = profit
+                    
+        return dp[0][0]
+        
+        """
+        #top down dp solution
         
         def dp(idx,status):
             if (idx,status) not in memo:
@@ -25,3 +48,8 @@ class Solution:
         max_profit = dp(0,'buy')
         
         return max_profit
+        
+        #time complexity: O(2n)== O(n)
+        #space complexity: O(2n) == O(n)
+        """
+        
