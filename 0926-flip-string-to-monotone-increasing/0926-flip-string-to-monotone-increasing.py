48fc20ca7dd83301c824afc12528eb6f8612e12f
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-01-17 12:53:29 +0300

    Time: 801 ms (21.16%), Space: 257.9 MB (5.13%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
@@ -1,17 +1,22 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
+        memo = {}
+        
         def dp(idx,zeros):
-            if idx == len(s) or zeros == 0:
-                return 0
-            
-            cur_flip = 0
-            
-            if s[idx] == '1':
-                cur_flip = min(zeros,1 + dp(idx+1,zeros))
-            else:
-                cur_flip = dp(idx+1,zeros-1)
+            if (idx,zeros) not in memo:
+                if idx == len(s) or zeros == 0:
+                    return 0
+
+                cur_flip = 0
+
+                if s[idx] == '1':
+                    cur_flip = min(zeros,1 + dp(idx+1,zeros))
+                else:
+                    cur_flip = dp(idx+1,zeros-1)
                 
-            return cur_flip
+                memo[(idx,zeros)] = cur_flip
+                
+            return memo[(idx,zeros)]
                 
         zeros = s.count('0')
         return dp(0,zeros)
