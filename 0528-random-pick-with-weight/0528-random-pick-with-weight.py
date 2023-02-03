b274f92c5ec6447a10c7c6900f78646729d7391e
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-02-03 19:05:33 +0300

    Time: 284 ms (48.03%), Space: 18.9 MB (12.27%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -7,10 +7,25 @@ class Solution:
         
         for val in self.w:
             self.prefix_sum.append(self.prefix_sum[-1]+val)
-
+            
+    def binarySearch(self,target):
+        left = 0
+        right = self.size - 1
+        
+        while left <= right:
+            mid = left + (right - left)//2
+            
+            if self.prefix_sum[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return left
+        
     def pickIndex(self) -> int:
         rand_num = random.randint(1,self.prefix_sum[-1])
-        return bisect.bisect_left(self.prefix_sum,rand_num) - 1
+        
+        return self.binarySearch(rand_num) - 1
 
 
 # Your Solution object will be instantiated and called as such:
