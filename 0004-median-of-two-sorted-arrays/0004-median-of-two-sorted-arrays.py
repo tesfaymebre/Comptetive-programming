149781da063d57d01ad5a0b1b361534f0383194e
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-02-10 22:57:24 +0300

    Time: 91 ms (85.20%), Space: 14.1 MB (58.91%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,11 +1,32 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        merged = nums1 + nums2
-        merged.sort()
+        def findMedian(arr1, arr2, mid):
+            if not arr1:
+                return arr2[mid]
+            
+            if not arr2:
+                return arr1[mid]
+            
+            median_idx1, median_idx2 = len(arr1) // 2 , len(arr2) // 2
+            median_val1, median_val2 = arr1[median_idx1], arr2[median_idx2]
+
+            if median_idx1 + median_idx2 < mid:
+                
+                if median_val1 > median_val2:
+                    return findMedian(arr1, arr2[median_idx2 + 1:], mid - median_idx2 - 1)
+                else:
+                    return findMedian(arr1[median_idx1 + 1:], arr2, mid - median_idx1 - 1)
+            else:
+                if median_val1 > median_val2:
+                    return findMedian(arr1[:median_idx1], arr2, mid)
+                else:
+                    return findMedian(arr1, arr2[:median_idx2], mid)
+            
+        total_size = len(nums1) + len(nums2)
         
-        mid = len(merged)//2
-        
-        if len(merged)&1:
-            return merged[mid]
+        if total_size&1:
+            return findMedian(nums1, nums2, total_size // 2)
         else:
-            return (merged[mid-1] + merged[mid])/2
+            return (findMedian(nums1,nums2,total_size // 2) + findMedian(nums1,nums2,total_size // 2 - 1)) / 2.   
+
+    
