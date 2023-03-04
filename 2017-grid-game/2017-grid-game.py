a742035ce7de1eb375406a995fc81363fb1e9b9f
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-03-04 20:39:57 +0300

    Time: 1166 ms (79.83%), Space: 30.3 MB (42.92%) - LeetHub

--- 2017-grid-game/2017-grid-game.py
+++ 2017-grid-game/2017-grid-game.py
@@ -1,25 +1,26 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
-        prefix_sum = [[0]*(len(grid[0])+1) for _ in range(3)]
+        n = len(grid[0])
         
-        rows = len(prefix_sum)
-        cols = len(prefix_sum[0])
+        sufix = [0]*(len(grid[0])+1)
+        prefix = [0]
         
-        for r in range(1,rows):
-            for c in range(1,cols):
-                prefix_sum[r][c] = prefix_sum[r-1][c] + prefix_sum[r][c-1] \
-                                  - prefix_sum[r-1][c-1] + grid[r-1][c-1]
-       
+        for i in range(n-1,-1,-1):
+            sufix[i] = sufix[i+1] + grid[0][i]
+            
+        for j in range(n):
+            prefix.append(prefix[-1] + grid[1][j])
+        
         robot2 = float('inf')
         
-        for i in range(1,cols):
-            remain_path_sum1 = prefix_sum[2][i-1] - prefix_sum[1][i-1]
-            remain_path_sum2 = prefix_sum[1][cols-1] - prefix_sum[1][i]
+        for turn in range(n):
+            remain1 = sufix[turn+1]
+            remain2 = prefix[turn]
+            posible_val = max(remain1,remain2)
             
-            if robot2 > max(remain_path_sum1,remain_path_sum2):
-                robot2 = max(remain_path_sum1,remain_path_sum2)
-        
+            robot2 = min(robot2,posible_val)
+            
         return robot2
-            
         
-                
+        
+        
