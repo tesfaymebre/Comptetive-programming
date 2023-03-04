f34df864f61dffc265673f31b732c64c29b4e4b9
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-03-04 20:49:58 +0300

    Time: 1277 ms (36.26%), Space: 31.5 MB (8.58%) - LeetHub

--- 2017-grid-game/2017-grid-game.py
+++ 2017-grid-game/2017-grid-game.py
@@ -2,20 +2,32 @@ class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
         n = len(grid[0])
         
-        sufix = [0]*(len(grid[0])+1)
+        sufix = [0]*(len(grid[0])+1) # time :n+1 space : n + 1
         prefix = [0]
         
-        for i in range(n-1,-1,-1):
-            sufix[i] = sufix[i+1] + grid[0][i]
+        """
+        [[11,9,4]
+        ,[1,6,7]]
+        """
+        for i in range(n-2,-1,-1):
+            grid[0][i] += grid[0][i+1] #time : n
             
-        for j in range(n):
-            prefix.append(prefix[-1] + grid[1][j])
+        for j in range(1,n):
+            grid[1][j] += grid[1][j-1]
         
         robot2 = float('inf')
         
         for turn in range(n):
-            remain1 = sufix[turn+1]
-            remain2 = prefix[turn]
+            remain1 = 0
+            
+            if turn < n - 1:
+                remain1 = grid[0][turn+1]
+                
+            remain2 = 0
+            
+            if turn > 0:
+                remain2 = grid[1][turn-1]
+                
             posible_val = max(remain1,remain2)
             
             robot2 = min(robot2,posible_val)
@@ -23,4 +35,6 @@ class Solution:
         return robot2
         
         
+        # time: O(n)
+        #space: O(1)
         
