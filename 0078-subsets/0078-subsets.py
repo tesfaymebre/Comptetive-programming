c5efee9ac9e17ac64e29bb33f75e36147dea3c0d
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-03-14 22:05:35 +0300

    Time: 32 ms (83.34%), Space: 14.1 MB (27.64%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,5 +1,25 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        #bit manipulation
+        n = len(nums)
+        nth_bit = 1 << n
+        ans = []
+
+        for i in range(2**n):
+            bit = bin(i | nth_bit)[3:] 
+            temp = []
+
+            for j in range(n):
+                if int(bit[j]):
+                    temp.append(nums[j])
+            ans.append(temp)
+
+        return ans
+                
+        
+        """
+        #backtrack solution
+        
         ans = []
         
         def backT(idx,path):
@@ -15,4 +35,5 @@ class Solution:
             
         backT(0,[])
         return ans
+        """
     
