cbacce58b153cfed33432326da0df3b21e5afee3
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-03-26 21:22:47 +0300

    Time: 34 ms (49.78%), Space: 13.9 MB (67.05%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,26 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        buttons = {'2':'abc','3':'def','4':'ghi','5':'jkl',
+                   '6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        
+        result = []
+        
+        def backT(index,path):
+            if index == len(digits):
+                if path:
+                    result.append(''.join(path))
+                return
+            
+            for c in buttons[digits[index]]:
+                backT(index+1,path + [c])
+                
+        backT(0,[])
+        return result
+        
+                
+    """
+    arr1 = [1,2]
+    arr1.extend([3,4])
+    
+ arr1 + [3,4]
+    """
