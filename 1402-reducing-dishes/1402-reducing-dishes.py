c1c179edf83a40b5bd1debd45d3ae038bb7e4604
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-03-29 12:53:00 +0300

    Time: 789 ms (14.87%), Space: 19.7 MB (21.84%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,6 +1,20 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        #bottom up dp solution
+        satisfaction.sort()
+        size = len(satisfaction)
+        dp = [[0]*(size+2) for _ in range(size+1)]
         
+        for idx in range(size-1,-1,-1):
+            for time in range(size,0,-1):
+                cook = satisfaction[idx]*time + dp[idx+1][time+1]
+                not_cook = dp[idx+1][time]
+                dp[idx][time] = max(cook,not_cook)
+                
+        return dp[0][1]
+        
+        """
+        #top down dp solution
         def dp(idx,time):
             if (idx,time) not in memo:
                 if idx >= len(satisfaction):
@@ -16,3 +30,4 @@ class Solution:
         memo = {}
         satisfaction.sort()
         return dp(0,1)
+        """
