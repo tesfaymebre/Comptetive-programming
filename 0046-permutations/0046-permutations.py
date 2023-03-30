b21f3fc6b7e4b000635b855d1b6239267742785a
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-03-30 12:13:06 +0300

    Time: 37 ms (86.29%), Space: 14.1 MB (49.63%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,6 +1,27 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        #using bit manipulation 
+        n = len(nums)
         self.res = []
+        
+        def backT(seen,path):
+            if len(path) == len(nums):
+                self.res.append(path[:])
+                return
+            
+            for i in range(len(nums)):
+                if seen & (1<<(n-i-1)) == 0:
+                    seen |= 1 << (n-i-1)
+                    backT(seen,path + [nums[i]])
+                    seen ^= 1 << (n-i-1)
+                    
+            return
+        
+        backT(0,[])
+        return self.res
+    
+    """
+    self.res = []
         
         def backT(seen,path):
             if len(path) == len(nums):
@@ -17,3 +38,4 @@ class Solution:
         
         backT(set(),[])
         return self.res
+    """
