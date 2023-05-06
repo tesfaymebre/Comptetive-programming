5f7567cba83880a327e55f7c2533062681649a46
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-05-06 23:54:40 +0300

    Time: 530 ms (80.73%), Space: 17.7 MB (17.43%) - LeetHub

---
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPossible(self, nums: List[int]) -> bool:
+        heap=[]
+        
+        for x in nums:
+            
+            while heap and (x>(heap[0][0]+1)):
+                length= heapq.heappop(heap)
+                if length[1]<3:
+                    return False
+            
+            if heap and x==(heap[0][0]+1):
+                length=heapq.heappop(heap)
+                heapq.heappush(heap,(x,length[1]+1))
+            else:
+                heapq.heappush(heap,(x,1))
+        
+        while heap:
+            if heapq.heappop(heap)[1]<3:
+                return False
+        
+        return True
