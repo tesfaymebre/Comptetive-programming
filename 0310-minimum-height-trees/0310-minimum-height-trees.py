67d4db7973262a174784455ecbd442b7b6397a72
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-05-12 22:20:02 +0300

    Time: 537 ms (42.87%), Space: 26.1 MB (37.86%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,5 +1,8 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n == 1:
+            return [0]
+        
         graph = defaultdict(list)
         degree = [0]*n
         
@@ -8,38 +11,29 @@ class Solution:
             graph[b].append(a)
             degree[a] += 1
             degree[b] += 1
-            
-        count = [0]*n
         
         queue = deque()
         
         for i in range(n):
             if degree[i] == 1:
-                queue.append((i,0))
+                queue.append(i)
         
-        while queue:
+        count_seen = len(queue)
+        
+        while queue and count_seen != n:
             size = len(queue)
             
             for _ in range(size):
-                curr,dist = queue.popleft()
+                curr = queue.popleft()
                 
-                for node in graph[curr]:
-                    degree[node] -= 1
+                for neighbour in graph[curr]:
+                    degree[neighbour] -= 1
                     degree[curr] -= 1
                     
-                    if degree[node] > 0:
-                        count[node] = max(count[node],1+dist)
-                    
-                    if degree[node] == 1:
-                        queue.append((node,count[node]+1))
+                    if degree[neighbour] == 1:
+                        queue.append(neighbour)
                         
-        height = max(count)
-        
-        ans = []
-        
-        for j in range(n):
-            if count[j] == height:
-                ans.append(j)
+            count_seen += len(queue)
                 
-        return ans
+        return queue
         
