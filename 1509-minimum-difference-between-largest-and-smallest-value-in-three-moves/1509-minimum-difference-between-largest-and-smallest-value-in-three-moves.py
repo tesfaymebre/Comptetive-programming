05b257df24e83832ca35b1d05df90d9bd161aa23
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-05-25 10:55:58 +0300

    Time: 348 ms (78.36%), Space: 27.7 MB (9.09%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -5,9 +5,9 @@ class Solution:
             return 0
         
         nums.sort()
-        first = max(nums[3:]) - min(nums[3:])
-        second = max(nums[:size-3]) - min(nums[:size-3])
-        third = max(nums[2:size-1]) - min(nums[2:size-1])
-        fourth = max(nums[1:size-2]) - min(nums[1:size-2])
-                    
-        return min(first,second,third,fourth)
+        smallest = float('inf')
+        
+        for i in range(4):
+            smallest = min(smallest,nums[i-4] - nums[i])
+            
+        return smallest
