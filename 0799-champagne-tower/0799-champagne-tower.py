0f9ab58f10a6c87a94bc5290f54d8977b2da86ef
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-06-27 13:01:11 +0300

    Time: 168 ms (38.63%), Space: 16.4 MB (75.11%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.py
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -1,30 +1,28 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-#         dp = []
-#         for index in range(query_row+2):
-#             temp = []
-#             for j in range(index+1):
-#                 temp.append(0)
-
-#             dp.append(temp)
-
-#         dp[0][0] = poured
-
-        dp = [[0] * (query_row + 2) for _ in range(query_row + 2)]
+        dp = [[0] * (query_row + 2) for _ in range(2)]
         dp[0][0] = poured
         
         lengthRow = query_row + 2
         for row in range(lengthRow):
             lengthCol = row+1
+            
+            if row + 1 < lengthRow:
+                 dp[(row + 1)&1][0] = 0
+                
             for col in range(lengthCol):
                 if row + 1 < lengthRow:
-                    if dp[row][col] >= 1:
-                        flow = dp[row][col] - 1
-                        dp[row][col] -= flow
-                        dp[row + 1][col] += flow/2
+                    if dp[row&1][col] >= 1:
+                        flow = dp[row&1][col] - 1
+                        dp[row&1][col] -= flow
+                        dp[(row + 1)&1][col] += flow/2
                         if col + 1 < row+2:
-                            dp[row+1][col+1] += flow/2
-
-       
-
-        return dp[query_row][query_glass]
+                            dp[(row+1)&1][col+1] = flow/2
+                            
+                    elif col + 1 < row+2:
+                        dp[(row+1)&1][col+1] = 0
+                            
+            if row == query_row:
+                return dp[row&1][query_glass]
+    
+  
