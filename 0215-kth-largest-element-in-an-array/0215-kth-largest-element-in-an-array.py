63f0aa45dabaec5873d887f0e5e960a5d8f3815f
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-08-14 23:09:11 +0300

    Time: 459 ms (85.62%), Space: 29.3 MB (84.82%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,39 +1,12 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        def partition(left, right):
-            # print(left, right)
-            # print(nums[left], nums[right], "the items")
-            if right - left <= 0:
-                if left == len(nums) - k:
-                    print('an',nums[left])
-                    return nums[left]
-                return 
-            
-            pivot = right
-            leftMost = left
-            while left < right:
-                if nums[left] < nums[pivot]:
-                    left += 1
-                else:
-                    if nums[right] >= nums[pivot]:
-                        right -= 1
-                    else:
-                        nums[left], nums[right] = nums[right], nums[left]
-            
-            nums[left], nums[pivot] = nums[pivot], nums[left]
-
-            if left == len(nums) - k:
-                return nums[left]
-            
-            leftHalf = partition(leftMost, left-1)
-            rightHalf = partition(left+1, pivot)
-            print(leftMost,rightHalf,leftHalf,'x')
-            
-            if leftHalf != None:
-                return leftHalf
-            return rightHalf
-            # return leftHalf or rightHalf
+        min_heap = []
         
-        ans = partition(0, len(nums) - 1)
-        print(ans)
-        return ans
+        for i in range(len(nums)):
+            
+            if len(min_heap) < k:
+                heappush(min_heap, nums[i])
+            elif min_heap[0] < nums[i]:
+                heapreplace(min_heap, nums[i])
+        
+        return min_heap[0]
