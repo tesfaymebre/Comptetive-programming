b69648d26ddacd8329695c46ecda6bebb562bc1f
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-09-04 10:02:39 +0300

    Time: 2627 ms (17.53%), Space: 511.3 MB (9.30%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        memo = {}
+        
+        def dp(i, transaction, bought):
+            if (i,transaction,bought) not in memo:
+                if i >= len(prices) or transaction == 0:
+                    return 0
+
+                # Skip
+                profit = dp(i + 1, transaction, bought);
+
+                # Sell
+                if bought:
+                    profit = max(profit, dp(i + 1, transaction - 1, not bought) + prices[i])
+                # Buy
+                else:
+                    profit = max(profit, dp(i + 1, transaction, not bought) - prices[i])
+
+                memo[(i,transaction,bought)] = profit
+                
+            return memo[(i,transaction,bought)]
+        
+        return dp(0, 2, False)
