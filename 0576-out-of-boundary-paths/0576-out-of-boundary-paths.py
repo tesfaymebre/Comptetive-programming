f71a5c8aa85100bbbcb2cc65776e756b95822c0d
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-09-04 11:44:15 +0300

    Time: 285 ms (10.58%), Space: 20.5 MB (65.87%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
@@ -1,6 +1,35 @@
 class Solution:
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
         M = 10**9 + 7
+        DIR = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+        inbound = lambda row, col: -1 < row < m and -1 < col < n
+
+        # Initialize a 3D DP table to store the number of paths
+        dp = [[[0 for _ in range(maxMove + 1)] for _ in range(n)] for _ in range(m)]
+
+        # Initialize the base case: starting position has 1 path
+        # dp[startRow][startColumn][0] = 1
+
+        for move in range(1, maxMove + 1):
+            for r in range(m):
+                for c in range(n):
+                    paths = 0
+                    for x, y in DIR:
+                        nx = r + x
+                        ny = c + y
+                        if inbound(nx, ny):
+                            paths += dp[nx][ny][move - 1]
+                        else:
+                            paths += 1  # Increment count for out-of-bounds moves
+                            
+                    dp[r][c][move] = paths % M
+                    
+        return dp[startRow][startColumn][maxMove] % M
+        
+        """
+        #top down dp solution
+        
+        M = 10**9 + 7
         DIR = [[0,1],[1,0],[-1,0],[0,-1]]
         inbound = lambda row,col: -1 < row < m and -1 < col < n
         memo = {}
@@ -25,6 +54,7 @@ class Solution:
             return memo[(r,c,maxMove)]
         
         return dp(startRow,startColumn,maxMove)
+        """
                     
                 
         
