77e67eb0ac6e8f10df4b23eee626ab51ddd6f322
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-09-04 13:02:55 +0300

    Time: 275 ms (57.22%), Space: 18.8 MB (65.23%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
@@ -1,5 +1,34 @@
 class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
+        #bottom up dp solution
+        DIR = [(1, 2), (1, -2), (-1, 2), (-1, -2),(2, 1), (2, -1), (-2, 1), (-2, -1)]
+        inbound = lambda row, col: -1 < row < n and -1 < col < n
+
+        # Initialize a 3D DP table to store the number of paths
+        dp = [[[0 for _ in range(k + 1)] for _ in range(n)] for _ in range(n)]
+        
+        #base case
+        for r in range(n):
+            for j in range(n):
+                dp[r][j][0] = 1
+
+        for move in range(1, k + 1):
+            for r in range(n):
+                for c in range(n):
+                    curr_probability = 0
+
+                    for x,y in DIR:
+                        nx = r+x
+                        ny = c+y
+
+                        if inbound(nx,ny):
+                            curr_probability += dp[nx][ny][move-1]
+
+                    dp[r][c][move] = curr_probability / 8
+                    
+        return dp[row][column][k]
+        
+        """
         #top down dp solution
         DIR = [(1, 2), (1, -2), (-1, 2), (-1, -2),(2, 1), (2, -1), (-2, 1), (-2, -1)]
         inbound = lambda row,col: -1 < row < n and -1 < col < n
@@ -24,3 +53,4 @@ class Solution:
             return memo[(r,c,k)]
 
         return dp(row,column,k)
+        """
