cfda94718bf5d7ccc7b3db66ef9c5f1bef525458
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-09-04 10:29:13 +0300

    Time: 2269 ms (21.28%), Space: 66.1 MB (19.25%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,5 +1,28 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        #bottom up dp solution
+
+        n = len(prices)
+        max_transactions = 2  
+        dp = [[[0 for _ in range(2)] for _ in range(max_transactions + 1)] for _ in range(n+1)]
+
+        for i in range(n-1,-1,-1):
+            for transaction in range(1,max_transactions + 1):
+                for bought in range(2):
+                    profit = dp[i+1][transaction][bought]
+                    
+                    if bought:
+                        profit = max(profit, dp[i + 1][transaction - 1][0] + prices[i])
+                    else:
+                        profit = max(profit, dp[i + 1][transaction][1] - prices[i])
+                        
+                    dp[i][transaction][bought] = profit
+        
+        return dp[0][2][0]
+
+        
+        """
+        # top  down dp solution
         memo = {}
         
         def dp(i, transaction, bought):
@@ -22,3 +45,4 @@ class Solution:
             return memo[(i,transaction,bought)]
         
         return dp(0, 2, False)
+        """
