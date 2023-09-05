651c835e6dccf515d7b0c8005477779b0235738d
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-09-05 17:38:47 +0300

    Time: 41 ms (87.23%), Space: 16.3 MB (62.05%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,5 +1,20 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
+        #bottom up dp solution
+        n = len(nums)
+        dp = [0]*(target+1)
+        
+        #base case
+        dp[0] = 1
+        
+        for amount in range(1,target+1):
+            for i in range(n):
+                if nums[i] <= amount:
+                    dp[amount] += dp[amount-nums[i]]
+
+        return dp[target]
+        
+        """
         #top down solution
         memo = {}
         
@@ -19,3 +34,4 @@ class Solution:
         
         return dp(target)
         
+        """
