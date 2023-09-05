34f35fdfa199303a7c37e18c4661d0f760858993
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-09-05 17:36:08 +0300

    Time: 47 ms (63.01%), Space: 16.5 MB (21.85%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,42 +1,21 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        #bottom up dp solution
-        n = len(nums)
-        dp = [[0]*(target+1) for _ in range(n)]
-        
-        #base case
-        for i in range(n):
-            dp[i][0] = 1
-        
-        for amount in range(1,target+1):
-            for idx in range(n):
-                count = 0
-                
-                for i in range(n):
-                    if nums[i] <= amount:
-                        count += dp[i][amount-nums[i]]
-                        
-                dp[idx][amount] = count
-                
-        return dp[0][target]
-    
-        """
         #top down solution
         memo = {}
         
-        def dp(idx,amount):
-            if (idx,amount) not in memo:
+        def dp(amount):
+            if amount not in memo:
                 if amount == 0:
                     return 1
                 
                 count = 0
                 for i in range(len(nums)):
                     if nums[i] <= amount:
-                        count += dp(i,amount-nums[i])
+                        count += dp(amount-nums[i])
                 
-                memo[(idx,amount)] = count
+                memo[amount] = count
                 
-            return memo[(idx,amount)]
+            return memo[amount]
         
-        return dp(0,target)
-        """
+        return dp(target)
+        
