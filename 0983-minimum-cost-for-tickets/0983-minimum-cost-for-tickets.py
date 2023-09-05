9da979392db375ae823b5c08172a7cc16d95c211
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2023-09-05 18:05:41 +0300

    Time: 55 ms (49.53%), Space: 16.3 MB (75.99%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,6 +1,23 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        #bottom up dp solution
+        n = len(days)
+        dp = [float('inf')]*(n+1)
+        dp[n] = 0
         
+        for idx in range(n-1,-1,-1):
+            for i,passes in enumerate([1,7,30]):
+                temp = bisect.bisect_left(days,days[idx]+passes-1)
+                
+                if temp < n and days[temp] == days[idx]+passes-1:
+                    dp[idx] = min(dp[idx],costs[i] + dp[temp+1])
+                else:
+                    dp[idx] = min(dp[idx],costs[i]+dp[temp])
+                    
+        return dp[0]
+        
+        """
+        #top down dp solution
         def dp(idx):
             if idx not in memo:
                 if idx >= len(days):
@@ -21,3 +38,4 @@ class Solution:
         
         memo = {}
         return dp(0)
+        """
