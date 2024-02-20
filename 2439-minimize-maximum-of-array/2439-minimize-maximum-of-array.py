5d901b7659c072c0b3a90fdbfad8653695f9a969
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2024-02-20 16:51:50 +0300

    Time: 607 ms (38.28%), Space: 29.5 MB (68.36%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -1,4 +1,23 @@
 class Solution:
+    def minimizeArrayValue(self, nums: List[int]) -> int:
+        # solution 2: greedy solution
+        min_max = 0
+        curr_total = 0
+        
+        for i in range(len(nums)):
+            curr_total += nums[i]
+            curr_average = ceil(curr_total / (i+1))
+            
+            min_max = max(min_max,curr_average)
+            
+        return min_max
+            
+            
+        
+"""
+solution 1: using binary search
+
+class Solution:
     def check(self,mid,nums):
         last = nums[-1]
         
@@ -26,3 +45,5 @@ class Solution:
                 left = mid + 1
                 
         return left
+"""
+        
