a8a07d68f1a086bf0e6e00744dca2ffe9babcbf6
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2024-03-07 10:27:59 +0300

    Time: 231 ms (36.36%), Space: 18.2 MB (46.07%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,6 +1,33 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        #solution 2 using binary search
+        if len(arr) == k:
+            return arr
+        
+        # Find the closest element and initialize two pointers
+        left = bisect_left(arr, x) - 1
+        right = left + 1
+
+        # While the window size is less than k
+        while right - left - 1 < k:
+            # Be careful to not go out of bounds
+            if left == -1:
+                right += 1
+                continue
+            
+            # Expand the window towards the side with the closer number
+            # Be careful to not go out of bounds with the pointers
+            if right == len(arr) or abs(arr[left] - x) <= abs(arr[right] - x):
+                left -= 1
+            else:
+                right += 1
+        
+        # Return the window
+        return arr[left + 1:right]
+        """
+        #solution 1
         arr.sort(key=lambda a: (abs(a-x),a))
         result = sorted(arr[:k])
         
         return result
+        """
