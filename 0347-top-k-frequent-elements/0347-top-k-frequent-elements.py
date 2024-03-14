9e83c31cc41f61f83c41dbfdafe3c1d1a3ccd14e
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2024-03-14 16:44:47 +0300

    Time: 86 ms (74.88%), Space: 22 MB (17.21%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,6 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # solution 2 using bucket sort
         
+        bucket = [[] for _ in range(len(nums) + 1)]
+        Count = Counter(nums)
+        
+        for num, freq in Count.items(): 
+            bucket[freq].append(num)
+            
+        flat_list = [item for sublist in bucket for item in sublist]
+        
+        return flat_list[-k:]
+        
+        """
+        # solution 1 using heap
         freq = Counter(nums)
         freq_key = []
         
@@ -11,4 +24,4 @@ class Solution:
                 heapq.heappushpop(freq_key, (f,key))
         
         return [x[1] for x in freq_key]
-        
+        """
