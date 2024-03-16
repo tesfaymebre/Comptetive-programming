bfad1b9ed4349a643e8e642acc994b5c0433f1a9
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2024-03-16 11:15:31 +0300

    Time: 1512 ms (7.97%), Space: 40.8 MB (5.01%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,29 +1,52 @@
 class Solution:
-    def merge(self,left_half,right_half):
-        arr = []
-        p1 = 0
-        p2 = 0
-        
-        while p1 < len(left_half) and p2 < len(right_half):
-            if left_half[p1] <= right_half[p2]:
-                arr.append(left_half[p1])
-                p1 += 1
-            else:
-                arr.append(right_half[p2])
-                p2 += 1
-                
-        arr += left_half[p1:] + right_half[p2:]
-        return arr
-    
-    def merge_sort(self,left,right,nums):
-        if left == right:
-            return [nums[left]]
-        
-        mid = left + (right-left)//2
-        left_half = self.merge_sort(left,mid,nums)
-        right_half = self.merge_sort(mid+1,right,nums)
-        
-        return self.merge(left_half,right_half)
-    
     def sortArray(self, nums: List[int]) -> List[int]:
-        return self.merge_sort(0,len(nums)-1,nums)    
+        temp_arr = [0] * len(nums)
+        
+        # Function to merge two sub-arrays in sorted order.
+        def merge(left: int, mid: int, right: int):
+            # Calculate the start and sizes of two halves.
+            start1 = left
+            start2 = mid + 1
+            n1 = mid - left + 1
+            n2 = right - mid
+
+            # Copy elements of both halves into a temporary array.
+            for i in range(n1):
+                temp_arr[start1 + i] = nums[start1 + i]
+            for i in range(n2):
+                temp_arr[start2 + i] = nums[start2 + i]
+
+            # Merge the sub-arrays 'in tempArray' back into the original array 'arr' in sorted order.
+            i, j, k = 0, 0, left
+            while i < n1 and j < n2:
+                if temp_arr[start1 + i] <= temp_arr[start2 + j]:
+                    nums[k] = temp_arr[start1 + i]
+                    i += 1
+                else:
+                    nums[k] = temp_arr[start2 + j]
+                    j += 1
+                k += 1
+
+            # Copy remaining elements
+            while i < n1:
+                nums[k] = temp_arr[start1 + i]
+                i += 1
+                k += 1
+            while j < n2:
+                nums[k] = temp_arr[start2 + j]
+                j += 1
+                k += 1
+
+        # Recursive function to sort an array using merge sort
+        def merge_sort(left: int, right: int):
+            if left >= right:
+                return
+            mid = (left + right) // 2
+            # Sort first and second halves recursively.
+            merge_sort(left, mid)
+            merge_sort(mid + 1, right)
+            # Merge the sorted halves.
+            merge(left, mid, right)
+    
+        merge_sort(0, len(nums) - 1)
+        return nums
