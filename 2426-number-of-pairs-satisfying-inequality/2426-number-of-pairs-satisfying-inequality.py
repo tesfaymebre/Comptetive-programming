172b2fe1adea272a4c4cd6633b5a13e8315fb9ea
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2024-03-22 12:40:42 +0300

    Time: 1121 ms (20.35%), Space: 41.2 MB (7.08%) - LeetHub

--- 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
@@ -1,52 +1,57 @@
 class Solution:
-    def __init__(self):
-        self.count = 0
-        
-    def countPairs(self,left_half,right_half,diff):
-        size1 = len(left_half)
-        size2 = len(right_half)
-        p1 = 0
-        p2 = 0
-        
-        while p1 < size1 and p2 < size2:
-            if left_half[p1] <= right_half[p2] + diff:
-                self.count += size2 - p2
-                p1 += 1
-            else:
-                p2 += 1
-                
-        return
-    
-    def merge(self,left_half,right_half):
-        arr = []
-        p1 = 0
-        p2 = 0
-        
-        while p1 < len(left_half) and p2 < len(right_half):
-            if left_half[p1] <= right_half[p2]:
-                arr.append(left_half[p1])
-                p1 += 1
-            else:
-                arr.append(right_half[p2])
-                p2 += 1
-                
-        arr += left_half[p1:] + right_half[p2:]
-        return arr
-    
-    def merge_sort(self,left,right,nums,diff):
-        if left == right:
-            return [nums[left]]
-        
-        mid = left + (right-left)//2
-        left_half = self.merge_sort(left,mid,nums,diff)
-        right_half = self.merge_sort(mid+1,right,nums,diff)
-        
-        self.countPairs(left_half,right_half,diff)
-        return self.merge(left_half,right_half)
-    
     def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
-        new_nums = [x-y for x,y in zip(nums1,nums2)]
+        arr = [ x - y for x,y in zip(nums1,nums2)]
+        count = 0
         
-        self.merge_sort(0,len(new_nums)-1,new_nums,diff)
-        return self.count
+        def count_pairs(left_half, right_half):
+            nonlocal count
+            
+            size_l = len(left_half)
+            size_r = len(right_half)
+            l = 0
+            r = 0
+            
+            while l < size_l and r < size_r:
+                if left_half[l] <= right_half[r] + diff:
+                    count += size_r - r
+                    l += 1
+                else:
+                    r += 1
+                    
+            return
+        
+        def merge(left_half, right_half):
+            size_l = len(left_half)
+            size_r = len(right_half)
+            l = 0
+            r = 0
+            
+            res = []
+            
+            while l < size_l and r < size_r:
+                if left_half[l] <= right_half[r]:
+                    res.append(left_half[l])
+                    l += 1
+                else:
+                    res.append(right_half[r])
+                    r += 1
+            
+            res.extend(left_half[l:])
+            res.extend(right_half[r:])
+            return res
+                    
+        
+        def merge_sort(left,right):
+            if left == right:
+                return [arr[left]]
+            
+            mid = left + (right - left) // 2
+            left_half = merge_sort(left, mid)
+            right_half = merge_sort(mid+1, right)
+            
+            count_pairs(left_half, right_half)
+            return merge(left_half,right_half)
+        
+        merge_sort(0, len(arr) - 1)
+        return count
         
