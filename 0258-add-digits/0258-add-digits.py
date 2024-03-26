91e08117c6890047e72168d51293fea7d8db56b3
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2024-03-26 15:20:46 +0300

    Time: 16 ms (99.98%), Space: 16.5 MB (88.96%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,5 +1,16 @@
 class Solution:
     def addDigits(self, num: int) -> int:
+        # solution 2
+        if num == 0:
+            return 0
+        
+        if num % 9 == 0:
+            return 9
+        
+        return num % 9
+        
+        """
+        # solution 1
         digital_root = 0
         while num > 0:
             digital_root += num % 10
@@ -10,3 +21,4 @@ class Solution:
                 digital_root = 0
                 
         return digital_root
+        """
