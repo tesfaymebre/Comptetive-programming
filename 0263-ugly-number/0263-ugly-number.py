85a52ff81cb06e88ce66b8175627bbb935e0d608
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2024-03-27 15:58:41 +0300

    Time: 45 ms (8.06%), Space: 16.5 MB (79.73%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -2,9 +2,17 @@ class Solution:
     def isUgly(self, n: int) -> bool:
         if n <= 0:
             return False
-    
-        for x in [2, 3, 5]:
-            while n % x == 0:
-                n = n / x
-                
-        return n == 1
+        
+        def keep_dividing(dividend, divisor):
+            while dividend % divisor == 0:
+                dividend //= divisor
+            
+            return dividend
+        
+        for divisor in [2,3,5]:
+            n = keep_dividing(n, divisor)
+            
+        if n == 1:
+            return True
+        
+        return False
