194572da85c38f2f00d275fd3a1dca7fcd741a38
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2024-05-22 21:23:55 +0300

    Time: 453 ms (85.55%), Space: 35.3 MB (41.59%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,44 +1,17 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        #bottom up dp solution
-        size = len(s)
-        dp = [[] for _ in range(size+1)]
-        dp[size] = [[]]
-        
-        for left in range(size-1,-1,-1):
-            result = []
+        n, ways = len(s), []
+
+        def visit(offset, way):
+            if offset == n:
+                ways.append(list(way))
             
-            for right in range(left,size):
-                if s[left:right+1] == s[left:right+1][::-1]:
-                    sub_partitions = dp[right+1]
-                    
-                    result += [[s[left:right+1]] + p for p in sub_partitions]
-                    
-            dp[left] = result[:]
+            for i in range(offset, n):
+                sub = s[offset:i + 1:]
+                if sub == sub[::-1]:
+                    way.append(sub)
+                    visit(i + 1, way)
+                    way.pop()
             
-        return dp[0]
-        
-        """
-        #top down dp solution
-        
-        def helper(left):
-            if left not in memo:
-                if left == len(s):
-                    return [[]]
-
-                result = []
-
-                for right in range(left,len(s)):
-                    if s[left:right+1] == s[left:right+1][::-1]:
-                        sub_partitions = helper(right+1)
-
-                        result += [[s[left:right+1]] + p for p in sub_partitions]
-
-                memo[left] = result
-            
-            return memo[left]
-        
-        memo = {}
-        
-        return helper(0)
-        """
+        visit(0, [])
+        return ways
