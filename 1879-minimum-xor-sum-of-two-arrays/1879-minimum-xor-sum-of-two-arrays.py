218ecf149a805e3e8e1e067f4566763359b98973
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2024-05-29 11:06:37 +0300

    Time: 3143 ms (5.51%), Space: 17.5 MB (77.06%) - LeetHub

--- 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.py
+++ 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.py
@@ -1,5 +1,31 @@
 class Solution:
     def minimumXORSum(self, nums1: List[int], nums2: List[int]) -> int:
+        # solution 3 space optimized bottom up dp
+        size = len(nums1)
+        
+        dp = []
+        
+        if size & 1:
+            dp.append([float('inf')]*(1 << size))
+            dp.append([0]*(1 << size))
+        else:
+            dp.append([0]*(1 << size))
+            dp.append([float('inf')]*(1 << size))
+            
+        
+        for idx1 in range(size - 1, -1, -1):
+            for mask in range((1 << size) - 1, -1 , -1):
+                xor_sum = float('inf')
+                
+                for idx2 in range(size):
+                    if (mask >> idx2) & 1 == 0:
+                        xor_sum = min(xor_sum, (nums1[idx1] ^ nums2[idx2]) + dp[(idx1 + 1) & 1][mask | (1 << idx2)])
+                        
+                dp[idx1 & 1][mask] = xor_sum
+                
+        return dp[0][0]
+    
+        """
         # solution 2 bottom up dp
         size = len(nums1)
         
@@ -17,6 +43,7 @@ class Solution:
                 dp[idx1][mask] = xor_sum
                 
         return dp[0][0]
+        """
         
         """
         # solution 1 top down dp
