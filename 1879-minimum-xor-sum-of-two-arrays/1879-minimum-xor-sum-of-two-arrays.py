7ba8b676d6a1e7d17d7ae37fb7d8d45837de51f2
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2024-05-29 11:00:53 +0300

    Time: 2978 ms (5.51%), Space: 25.5 MB (66.97%) - LeetHub

--- 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.py
+++ 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.py
@@ -1,5 +1,25 @@
 class Solution:
     def minimumXORSum(self, nums1: List[int], nums2: List[int]) -> int:
+        # solution 2 bottom up dp
+        size = len(nums1)
+        
+        dp = [[float('inf')]*(1 << size) for _ in range(size)]
+        dp.append([0]*(1 << size))
+        
+        for idx1 in range(size - 1, -1, -1):
+            for mask in range((1 << size) - 1, -1 , -1):
+                xor_sum = float('inf')
+                
+                for idx2 in range(size):
+                    if (mask >> idx2) & 1 == 0:
+                        xor_sum = min(xor_sum, (nums1[idx1] ^ nums2[idx2]) + dp[idx1 + 1][mask | (1 << idx2)])
+                        
+                dp[idx1][mask] = xor_sum
+                
+        return dp[0][0]
+        
+        """
+        # solution 1 top down dp
         memo = {}
         
         def dp(idx1, mask):
@@ -18,3 +38,4 @@ class Solution:
             return memo[(idx1, mask)]
         
         return dp(0, 0)
+        """
