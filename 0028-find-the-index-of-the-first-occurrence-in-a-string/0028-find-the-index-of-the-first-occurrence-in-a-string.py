6585985813d9b5259af7ee95b872ab3c4308f3ca
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2024-10-17 09:09:44 +0300

    Time: 39 ms (24.9%), Space: 16.6 MB (34%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,3 +1,43 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
+        if len(haystack) < len(needle):
+            return -1
+
+        pattern = 0
+        k = len(needle) - 1
+
+        for c in needle:
+            idx = ord(c) - ord('a') + 1
+            pattern += idx * pow(27,k)
+            k -= 1
+
+        window_hash = 0
+        k = len(needle) - 1
+
+        for i in range(len(needle)):
+            idx = ord(haystack[i]) - ord('a') + 1
+            window_hash += idx * pow(27,k)
+            k -= 1
+
+        if window_hash == pattern:
+            return 0
+
+        left = 0
+        right = len(needle)
+        k = len(needle) - 1
+        while right < len(haystack):
+            idx = ord(haystack[left]) - ord('a') + 1
+            window_hash -= idx * pow(27,k)
+
+            idx = ord(haystack[right]) - ord('a') + 1
+            window_hash *= 27
+            window_hash += idx
+
+            if window_hash == pattern:
+                return left + 1
+
+            left += 1
+            right += 1
+
+        return -1
+        
