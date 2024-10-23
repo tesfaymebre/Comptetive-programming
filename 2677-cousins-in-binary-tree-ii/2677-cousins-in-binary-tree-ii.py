5f10a365ee64bc412125c2fc1f2655bbf24890b5
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2024-10-23 12:05:30 +0300

    Time: 77 ms (100%), Space: 57.3 MB (82.17%) - LeetHub

--- 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.py
+++ 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.py
@@ -6,49 +6,29 @@
 #         self.right = right
 class Solution:
     def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        total = defaultdict(int)
-        level = 0
+        prev_level_sum = root.val
         queue = deque([root])
 
         while queue:
+            new_level_sum = 0
             size = len(queue)
 
             for _ in range(size):
                 node = queue.popleft()
-                total[level] += node.val
+                node.val = prev_level_sum - node.val
 
                 if node.left:
+                    new_level_sum += node.left.val
                     queue.append(node.left)
 
                 if node.right:
+                    new_level_sum += node.right.val
                     queue.append(node.right)
 
-            level += 1
+                if node.left and node.right:
+                    node.left.val += node.right.val
+                    node.right.val = node.left.val
 
-        root.val = 0
-        queue.append(root)
-        level = 1
-
-        while queue:
-            size = len(queue)
-
-            for _ in range(size):
-                node = queue.popleft()
-                temp = 0
-                if node.left:
-                    queue.append(node.left)
-                    temp += node.left.val
-
-                if node.right:
-                    queue.append(node.right)
-                    temp += node.right.val
-
-                if node.left:
-                    node.left.val = total[level] - temp
-
-                if node.right:
-                    node.right.val = total[level] - temp
-
-            level += 1
+            prev_level_sum = new_level_sum
 
         return root
