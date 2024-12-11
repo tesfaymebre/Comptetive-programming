190871f4a89f4a1b43b291493118a122b0466067
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2024-12-11 20:07:17 +0300

    Time: 276 ms (32.9%), Space: 28.6 MB (26.95%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.py
@@ -1,11 +1,11 @@
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
         maxi = max(nums)
-        prefix = [0] * (maxi + k + 2)
+        prefix = [0] * (maxi + 2)
 
         for num in nums:
             prefix[max(0,num - k)] += 1
-            prefix[num + k + 1] -= 1
+            prefix[min(maxi,num + k) + 1] -= 1
 
         for i in range(1,len(prefix)):
             prefix[i] += prefix[i-1]
