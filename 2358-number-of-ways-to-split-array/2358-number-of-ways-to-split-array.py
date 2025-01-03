5f16b83c3c14918c56a0ed22b4e446c9627fad7f
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2025-01-03 23:26:21 +0300

    Time: 46 ms (83.37%), Space: 32.3 MB (64.57%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
@@ -1,5 +1,17 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
+        total = sum(nums)
+        left_sum = 0
+        count = 0
+
+        for i in range(len(nums)-1):
+            left_sum += nums[i]
+
+            if left_sum >= total - left_sum:
+                count += 1
+
+        return count
+    """
         prefix = [0]
 
         for num in nums:
@@ -12,3 +24,4 @@ class Solution:
                 count += 1
 
         return count
+    """
