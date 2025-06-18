2f7ca00265b344957fab70f64479a1afbb83aec5
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2025-06-18 14:19:59 +0300

    Time: 308 ms (12.1%), Space: 24 MB (34.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,3 +1,55 @@
+class DSU:
+    def __init__(self,size):
+        self.parent = [i for i in range(size)]
+        self.rank = [1] * size
+
+    def find(self, target):
+        if self.parent[target] != target:
+            self.parent[target] = self.find(self.parent[target])
+
+        return self.parent[target]
+
+    def union(self, x, y):
+        p_x = self.find(x)
+        p_y = self.find(y)
+
+        if p_x == p_y:
+            return False
+
+        if self.rank[p_y] > self.rank[p_x]:
+            p_x, p_y = p_y, p_x
+
+        self.parent[p_y] = p_x
+        self.rank[p_x] += self.rank[p_y]
+        
+        return True
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        dsu = DSU(rows * cols)
+
+        DIR = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        in_bound = lambda r,c: -1 < r < rows and -1 < c < cols
+
+        islands = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':
+                    islands += 1
+
+                    for x,y in DIR:
+                        nr = r + x
+                        nc = c + y
+
+                        if in_bound(nr,nc) and grid[nr][nc] == '1':
+                            if dsu.union(r * cols + c, nr * cols + nc):
+                                islands -= 1
+
+        return islands
+
+"""
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         #1 bfs solution
@@ -49,3 +101,4 @@ class Solution:
 #                     self.count += 1
                     
 #         return self.count
+"""
