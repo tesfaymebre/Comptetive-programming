50ef73f7f04a84006ad5b01abd446fede7318555
Tesfamichael Asfaw <95352714+tesfaymebre@users.noreply.github.com>
2026-05-27 10:23:01 -0400

    Time: 260 ms (55.88%), Space: 21.7 MB (28.82%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -10,7 +10,7 @@ class Solution:
                 first_idx_upper[ord(c) - ord('A')] = i
 
         count = 0
-        print(last_idx_lower, first_idx_upper)
+        
         for j in range(26):
             if last_idx_lower[j] != -1 and first_idx_upper[j] != -1:
                 count += last_idx_lower[j] < first_idx_upper[j]
